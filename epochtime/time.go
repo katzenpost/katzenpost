e58e756c1e6766c9048fbe1474ebd99949b8ed48
Yawning Angel <yawning@schwanenlied.me>
2017-08-02 09:24:54 +0000

    epochtime: Add a routine for querying epoch related timekeeping.

---
+++ epochtime/time.go
@@ -0,0 +1,43 @@
+// time.go - Katzenpost epoch time.
+// Copyright (C) 2017  Yawning Angel.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Package epochtime implements Katzenpost epoch related timekeeping functions.
+package epochtime
+
+import "time"
+
+const period = 3 * time.Hour
+
+var epoch = time.Date(2017, 6, 1, 0, 0, 0, 0, time.UTC)
+
+// Now returns the current Katzenpost epoch, time since the start of the
+// current, and time till the next epoch.
+func Now() (current uint64, elapsed, till time.Duration) {
+	// Cache now for a consistent value for this query.
+	now := time.Now()
+
+	fromEpoch := time.Since(epoch)
+	if fromEpoch < 0 {
+		panic("epochtime: BUG: system time appears to predate the epoch")
+	}
+
+	current = uint64(fromEpoch / period)
+
+	base := epoch.Add(time.Duration(current) * period)
+	elapsed = now.Sub(base)
+	till = base.Add(period).Sub(now)
+	return
+}
---
+++ epochtime/time_test.go
@@ -0,0 +1,33 @@
+// time_test.go - Epoch time tests.
+// Copyright (C) 2017  Yawning Angel.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package epochtime
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestEpochTime(t *testing.T) {
+	require := require.New(t)
+
+	var now uint64
+	var elapsed, till time.Duration
+	require.NotPanics(func() { now, elapsed, till = Now() }, "Basic Now() sanity check")
+	t.Logf("Epoch: %v, Elapsed: %v Till: %v", now, elapsed, till)
+}
