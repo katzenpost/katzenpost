054e6b2d6cce81882f46a8045a9a6749b20f1e3c
Yawning Angel <yawning@schwanenlied.me>
2017-12-01 02:59:43 +0000

    packet: "Fix" the pool allocator issues.
    This should always work, but it's slightly inefficient.

--- packet.go
+++ packet.go
@@ -36,7 +36,7 @@ var (
 	rawPacketPool = sync.Pool{
 		New: func() interface{} {
 			b := make([]byte, constants.PacketLength)
-			return &b
+			return b
 		},
 	}
 	pktID uint64
@@ -126,7 +126,7 @@ func (pkt *packet) copyToRaw(b []byte) error {
 
 	// The common case of standard packet sizes uses a pool allocator
 	// to store the raw packets.
-	pkt.raw = *rawPacketPool.Get().(*[]byte)
+	pkt.raw = rawPacketPool.Get().([]byte)
 
 	// Sanity check, just in case the pool allocator is doing something dumb.
 	if len(pkt.raw) != len(b) {
@@ -142,7 +142,7 @@ func (pkt *packet) copyToRaw(b []byte) error {
 func (pkt *packet) disposeRaw() {
 	if len(pkt.raw) == constants.PacketLength {
 		utils.ExplicitBzero(pkt.raw)
-		rawPacketPool.Put(&pkt.raw)
+		rawPacketPool.Put(pkt.raw)
 	}
 	pkt.raw = nil
 }
