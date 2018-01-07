ce8e15035382d1124709102ac2797e525904ab75
David Stainton <dstainton415@gmail.com>
2018-01-07 19:14:48 +0000

    pki: add Deserialize method to Client interface

--- pki/pki.go
+++ pki/pki.go
@@ -217,9 +217,12 @@ type MixDescriptor struct {
 
 // Client is the abstract interface used for PKI interaction.
 type Client interface {
-	// Get returns the PKI document for the provided epoch.
-	Get(ctx context.Context, epoch uint64) (*Document, error)
+	// Get returns the PKI document along with the raw serialized form for the provided epoch.
+	Get(ctx context.Context, epoch uint64) (*Document, []byte, error)
 
 	// Post posts the node's descriptor to the PKI for the provided epoch.
 	Post(ctx context.Context, epoch uint64, signingKey *eddsa.PrivateKey, d *MixDescriptor) error
+
+	// Deserialize returns PKI document given the raw bytes.
+	Deserialize(raw []byte) (*Document, error)
 }
