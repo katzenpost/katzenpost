bb6a91faafe2da9e0a9fa9fe60873eb902692b5d
David Stainton <dstainton415@gmail.com>
2018-02-23 11:27:31 +0000

    voting: fix TestDocument

--- voting/internal/s11n/document_test.go
+++ voting/internal/s11n/document_test.go
@@ -105,7 +105,7 @@ func TestDocument(t *testing.T) {
 	t.Logf("signed document: '%v':", signed)
 
 	// Validate and deserialize.
-	ddoc, err := VerifyAndParseDocument([]byte(signed), k.PublicKey())
+	ddoc, _, err := VerifyAndParseDocument([]byte(signed), k.PublicKey())
 	require.NoError(err, "VerifyAndParseDocument()")
 	require.Equal(doc.Epoch, ddoc.Epoch, "VerifyAndParseDocument(): Epoch")
 
