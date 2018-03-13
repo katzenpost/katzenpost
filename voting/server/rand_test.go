e0cdb59a25122e7f47a19a4bbae5f8ec04dbc61c
Masala <masala@riseup.net>
2018-03-13 09:13:17 -0400

    verify that two DeterministicRandReaders agree

--- voting/server/rand_test.go
+++ voting/server/rand_test.go
@@ -26,19 +26,29 @@ import (
 func TestPerm(t *testing.T) {
 	assert := assert.New(t)
 
-	rand, err := NewDeterministicRandReader("47ade5905376604cde0b57e732936b4298281c8a67b6a62c6107482eb69e2941")
+	skey := "47ade5905376604cde0b57e732936b4298281c8a67b6a62c6107482eb69e2941"
+	rand, err := NewDeterministicRandReader(skey)
+	assert.NoError(err, "wtf")
+	rand2, err := NewDeterministicRandReader(skey)
 	assert.NoError(err, "wtf")
 
-	tmp := [6]byte{}
-	_, err = rand.Read(tmp[:])
-	assert.NoError(err, "wtf")
-	t.Logf("rand val is %x", tmp)
+	for i := 0; i < 42; i++ {
+		tmp := [6]byte{}
+		tmp2 := [6]byte{}
+		_, err = rand.Read(tmp[:])
+		assert.NoError(err, "wtf")
+		_, err = rand2.Read(tmp2[:])
+		assert.NoError(err, "wtf")
+		assert.True(tmp == tmp2)
+		t.Logf("rand values are %x", tmp)
+	}
 	for i := 1; i < 42; i++ {
 		j := rand.Int63()
 		t.Logf("%v", j)
 		assert.True(j>=0)
 	}
 	for i := 0; i < 42; i++ {
-		t.Logf("%v", rand.Perm(i))
+		p := rand.Perm(i)
+		t.Logf("%v %v %v", i, len(p), p)
 	}
 }
