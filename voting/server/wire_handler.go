bf63a54d714acce9e7c0ef13ae856fc40f85d82d
David Stainton <dstainton415@gmail.com>
2018-05-17 22:46:38 +0000

    Update copyright banners

--- voting/server/wire_handler.go
+++ voting/server/wire_handler.go
@@ -1,5 +1,5 @@
 // wire_handler.go - Katzenpost non-voting authority connection handler.
-// Copyright (C) 2018  Yawning Angel.
+// Copyright (C) 2017, 2018  Yawning Angel, masala and David Stainton.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
