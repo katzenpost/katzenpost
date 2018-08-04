a0e36f5356c89d1312788a30415a86a17c9edeee
David Stainton <dstainton415@gmail.com>
2018-08-04 03:12:17 +0100

    Add echo service plugin

---
+++ echo/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/hashicorp/go-plugin"
+	common "github.com/katzenpost/server/common-plugin"
+)
+
+type Echo struct{}
+
+func (Echo) OnRequest(payload string) (string, error) {
+	return payload, nil
+}
+
+func main() {
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: common.Handshake,
+		Plugins: map[string]plugin.Plugin{
+			"echo": &common.KaetzchenPlugin{Impl: &Echo{}},
+		},
+
+		// A non-nil value here enables gRPC serving for this plugin...
+		GRPCServer: plugin.DefaultGRPCServer,
+	})
+}
