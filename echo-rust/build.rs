c4783a361694aacc13e49496cea47a03a4632331
David Stainton <dstainton415@gmail.com>
2018-08-06 09:17:56 +0100

    echo-rust: fix build.rs to output to correct directory

--- echo-rust/build.rs
+++ echo-rust/build.rs
@@ -2,7 +2,7 @@ extern crate protoc_rust_grpc;
 
 fn main() {
     protoc_rust_grpc::run(protoc_rust_grpc::Args {
-        out_dir: "src",
+        out_dir: "src/proto",
         includes: &["../../server/common-plugin/proto/"],
         input: &["../../server/common-plugin/proto/kaetzchen.proto"],
         rust_protobuf: true,
