katzenpost/core

Add cryptographic certificate module
Note that we modify crypto/eddsa to implement some
additional methods in our Signer and Verifier interface types.

crypto/cert/ed25519_vectors_test.go
@@ -1,0 +1,162 @@
+// ed25519_vectors_test.go - ed25519 certificate test vectors.
+// Copyright (C) 2018  David Stainton.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package cert
+
+import (
+	"encoding/hex"
+	"testing"
+	"time"
+
+	"github.com/katzenpost/core/crypto/eddsa"
+	"github.com/stretchr/testify/assert"
+)
+
+type inTest struct {
+	signingKey string
+	toSign     string
+}
+
+type outTest struct {
+	payload string
+}
+
+func TestEd25519SingleSignatureCertificateVectors(t *testing.T) {
+	assert := assert.New(t)
+
+	certificateTests := []struct {
+		in   inTest
+		want outTest
+	}{
+		{
+			inTest{
+				signingKey: "e818ee275ddb72b8e63758dfba3a90e0f5687dd59f28c2812e9861ba19fa33bffb731cf47b3732b24a5f9c00a0304b66d461b23e7292c5eb406ec09adc2d95e0",
+				toSign:     "32d4f52620a57aa2b02564c296c0b4b0dbeca5471704a9f40000706bcc134d2f",
+			}, outTest{
+				payload: "a669436572746966696564582032d4f52620a57aa2b02564c296c0b4b0dbeca5471704a9f40000706bcc134d2f6a45787069726174696f6e1b0000000468912f00674b65795479706567656432353531396a5369676e61747572657381a2684964656e746974795820fb731cf47b3732b24a5f9c00a0304b66d461b23e7292c5eb406ec09adc2d95e0675061796c6f61645840ce49d7bde5bb504bedaaccbecfdd9e0f38afc7a301d681e4d5ee9025ecee68845aad35750a0edf6ef27da023c2a7ff395712fc38cd072751590a08e8e4ba7207645479706569617574686f726974796756657273696f6e00",
+			},
+		},
+		{
+			inTest{
+				signingKey: "f99bdb809088a609c270f612a0e44844d03d86a63e109966ca66f6678cc02a1065efbc72434d921af5285c0fd28af6ed8592c0ac44c834d9d98f5589ea21c03f",
+				toSign:     "ea7b8ef81c91986d93f0532c1de06ce25373c20782d8106ac6a4d85eee4802bb",
+			}, outTest{
+				payload: "a6694365727469666965645820ea7b8ef81c91986d93f0532c1de06ce25373c20782d8106ac6a4d85eee4802bb6a45787069726174696f6e1b0000000468912f00674b65795479706567656432353531396a5369676e61747572657381a2684964656e74697479582065efbc72434d921af5285c0fd28af6ed8592c0ac44c834d9d98f5589ea21c03f675061796c6f616458404282d5830ee49768f4fcfb3628413cc3450b831402ed96c912a41d20cda8f0f0d1bf36c9ef83fc2476f936e232f0dd157ee44979a33bed22fb9a5362353b9009645479706569617574686f726974796756657273696f6e00",
+			},
+		},
+		{
+			inTest{
+				signingKey: "13a40a6146651427a769e3dd17dc86d4497f5379aa077488fc8d40ce062675bf39b956e6aae2b76a13dd83c0eb96c3e2b22e6ad4e944625a7c0230b4561b82d9",
+				toSign:     "220b7f2efa95f089a75711aad6c843d9899af3e622f7adf06da6d7264ed0ece5093f423dc1fb5d1672643fa133680e49ce74ad5c0367ba0aee902b41a2ad3e9ffbb8db5503b33f0ae61ca28a01bf1b8615d8c2c5f9ac02572e89c76aef5c8b00c052ba4b13ef043fc9f39b90eed1e2e9bccc06a67d3cde025e0b7cfc15cc3efacc50b7bbc525e02f5c52f920b9e03bef07b320f37347359888feaed0ab4d83e0cc6877f00162b3ac424df7cee0b47f1a9f00d49651c2982e59504c889182cda7130e7f83949e193f",
+			}, outTest{
+				payload: "a66943657274696669656458c8220b7f2efa95f089a75711aad6c843d9899af3e622f7adf06da6d7264ed0ece5093f423dc1fb5d1672643fa133680e49ce74ad5c0367ba0aee902b41a2ad3e9ffbb8db5503b33f0ae61ca28a01bf1b8615d8c2c5f9ac02572e89c76aef5c8b00c052ba4b13ef043fc9f39b90eed1e2e9bccc06a67d3cde025e0b7cfc15cc3efacc50b7bbc525e02f5c52f920b9e03bef07b320f37347359888feaed0ab4d83e0cc6877f00162b3ac424df7cee0b47f1a9f00d49651c2982e59504c889182cda7130e7f83949e193f6a45787069726174696f6e1b0000000468912f00674b65795479706567656432353531396a5369676e61747572657381a2684964656e74697479582039b956e6aae2b76a13dd83c0eb96c3e2b22e6ad4e944625a7c0230b4561b82d9675061796c6f616458408385d9724572ff359e1d1e48a788dd07b8af34f8ea365484455a0342c9739574f45e15939f638ee8f26381e0e55b3e51578280b974d7190fb2746550ca5edd07645479706569617574686f726974796756657273696f6e00",
+			},
+		},
+	}
+
+	for _, test := range certificateTests {
+		// expires 600 years after unix epoch
+		expiration := time.Unix(18934214400, 0).Unix()
+		signingKeyRaw, err := hex.DecodeString(test.in.signingKey)
+		assert.NoError(err)
+		toSign, err := hex.DecodeString(test.in.toSign)
+		assert.NoError(err)
+		signingKey := new(eddsa.PrivateKey)
+		signingKey.FromBytes(signingKeyRaw)
+		certificate, err := Sign(signingKey, toSign, "authority", expiration)
+		assert.NoError(err)
+		payload, err := hex.DecodeString(test.want.payload)
+		assert.NoError(err)
+		assert.Equal(certificate, payload)
+	}
+}
+
+type multiSigTest struct {
+	signingKeys []string
+	toSign      string
+}
+
+func TestEd25519MultipleSignatureCertificateVectors(t *testing.T) {
+	assert := assert.New(t)
+
+	certificateTests := []struct {
+		in   multiSigTest
+		want outTest
+	}{
+		{
+			multiSigTest{
+				signingKeys: []string{
+					"e818ee275ddb72b8e63758dfba3a90e0f5687dd59f28c2812e9861ba19fa33bffb731cf47b3732b24a5f9c00a0304b66d461b23e7292c5eb406ec09adc2d95e0",
+					"f99bdb809088a609c270f612a0e44844d03d86a63e109966ca66f6678cc02a1065efbc72434d921af5285c0fd28af6ed8592c0ac44c834d9d98f5589ea21c03f",
+				},
+				toSign: "32d4f52620a57aa2b02564c296c0b4b0dbeca5471704a9f40000706bcc134d2f",
+			}, outTest{
+				payload: "a669436572746966696564582032d4f52620a57aa2b02564c296c0b4b0dbeca5471704a9f40000706bcc134d2f6a45787069726174696f6e1b0000000468912f00674b65795479706567656432353531396a5369676e61747572657382a2684964656e74697479582065efbc72434d921af5285c0fd28af6ed8592c0ac44c834d9d98f5589ea21c03f675061796c6f61645840bfd3096d056c30b8326247a22c3f464bde44c6197dc18e8f46590d3c6b943ac97938b791aa329d251df9f00e49d3aabb51751df9e512b65f4339401da201a50fa2684964656e746974795820fb731cf47b3732b24a5f9c00a0304b66d461b23e7292c5eb406ec09adc2d95e0675061796c6f61645840ce49d7bde5bb504bedaaccbecfdd9e0f38afc7a301d681e4d5ee9025ecee68845aad35750a0edf6ef27da023c2a7ff395712fc38cd072751590a08e8e4ba7207645479706569617574686f726974796756657273696f6e00",
+			},
+		},
+		{
+			multiSigTest{
+				signingKeys: []string{
+					"e818ee275ddb72b8e63758dfba3a90e0f5687dd59f28c2812e9861ba19fa33bffb731cf47b3732b24a5f9c00a0304b66d461b23e7292c5eb406ec09adc2d95e0",
+					"f99bdb809088a609c270f612a0e44844d03d86a63e109966ca66f6678cc02a1065efbc72434d921af5285c0fd28af6ed8592c0ac44c834d9d98f5589ea21c03f",
+					"13a40a6146651427a769e3dd17dc86d4497f5379aa077488fc8d40ce062675bf39b956e6aae2b76a13dd83c0eb96c3e2b22e6ad4e944625a7c0230b4561b82d9",
+				},
+				toSign: "32d4f52620a57aa2b02564c296c0b4b0dbeca5471704a9f40000706bcc134d2f",
+			}, outTest{
+				payload: "a669436572746966696564582032d4f52620a57aa2b02564c296c0b4b0dbeca5471704a9f40000706bcc134d2f6a45787069726174696f6e1b0000000468912f00674b65795479706567656432353531396a5369676e61747572657383a2684964656e74697479582039b956e6aae2b76a13dd83c0eb96c3e2b22e6ad4e944625a7c0230b4561b82d9675061796c6f61645840a3b8569c85dca832e24f935c37f71a7fd1e7165da9a09d53689e6944a99fbad0221dfb09080bbe788053c1fe5465b56f5079db9dc87bc00db3064fdf2ed50a06a2684964656e74697479582065efbc72434d921af5285c0fd28af6ed8592c0ac44c834d9d98f5589ea21c03f675061796c6f61645840bfd3096d056c30b8326247a22c3f464bde44c6197dc18e8f46590d3c6b943ac97938b791aa329d251df9f00e49d3aabb51751df9e512b65f4339401da201a50fa2684964656e746974795820fb731cf47b3732b24a5f9c00a0304b66d461b23e7292c5eb406ec09adc2d95e0675061796c6f61645840ce49d7bde5bb504bedaaccbecfdd9e0f38afc7a301d681e4d5ee9025ecee68845aad35750a0edf6ef27da023c2a7ff395712fc38cd072751590a08e8e4ba7207645479706569617574686f726974796756657273696f6e00",
+			},
+		},
+		{
+			multiSigTest{
+				signingKeys: []string{
+					"b82b2fbb1fa8fbf771dc85dc45e66a1626d44cf3361f3a61e387b266445346dec57a0f83107b9ded621900615ca8ad5fda6f7bc839fb0fbf6f4b6af59611c710",
+					"4b888f00728a6b7594f8eb6440e4a084889017d2d520ac7b3a8d0b5030872329d12f2a5061f93b378b94f72ddb2082e3e18681d902d4c65ae95c41001e8821be",
+					"e5ac1b84e158d06ace83947a5fba08af9b000d195503dae4d4a9728b5c0cef9ae896881e859d836443a4c6dfb7fbd3cd07160065b8eb000a4297c7c5c194e287",
+					"e66cc68c6606f1072485164e6ff989abaf1cef493ba25e922bbcb601769dc4dca3bfdb632fdc227a276e17b2b573d31c7e6bbb236c25af5c5ffde17139d9e4bd",
+				},
+				toSign: "2f15f386b80725ad11e1b2b55b53eeaea003a6cc6e5c359f344fb7af6f39dc043140578009262ea3247aef5e84c0a79e5962506185b54601b93d6d53bdc64dbcb27543657d2229096276a0e677f61cff7cb7c04bfc453b377bc780acb3163346cf3bcaffd0355ca29a6e47a981d85631b6c7c4bef767502d6982dc176df9776b217c2fe62886b139e48ea658c9aa450d995cc6d1bae5a220187694d120fa43fc77be3b3e57ea1a2d1f4247e6a580b8529063eb5dc44c5d64bab43474c66994bb36d149dfee4660ce5e1b8dc9edf8ca407a21519ecfdda6e7c3c3fd83257dc77e02f9c20cc1d68f56255a8e7147b3b6b334039d520215b5d219e899ebd454f3ba1f502f2b9710cc4cde4ec09d6ece17c19ee101eb42459cd3dfc1d7de76b58d4b0fed25335cb0756ea7eb2e762bc139ab8ec7ac8ea4240e034a95011649c5f856ed49e803f41e846a9043b320690b272d3b236af33e8a7d5fbb08a62edca052a58db32143c7b290a129303633ba944f5b9b66ce0123428bb20e7d8d26fb24bf0f5c2fa72a703d8bd31756ae540d887529832e48d8780efcab4e5c2cb59c89d853905562fd76920fd53f415e9cbdb6417a89de8b6ecf71051e877f2a230cee85b150f1479573b01ee7e486e2240b104df54f1dcfb469b946a6547eab1c32631d4171eb008d829231ce18155391570de540b1872a42c8547550f4b53a27430afa517784f32f849181243fc1ba3781caa4031829d491aa9b094ec6516c96880b013a25e4c94ad127452c0df9bd3ddb7eb108a7bfe65fee0626097c55c9a4ff55ac1a6ba5b85ae13408cb5d3e9a64bfbaa1848112f95ffc409229928ebcedde1ff4379ce69141d95dfc1fb10466d6fdfbeee2cccf961b71f2c59824ad43bf05ca9b0d5e48182f83cc61671354c72dfffb4a755cc44d3bd959078078a84ebfcf5a3817e820809ed87eb8d66adc5da1d55c33d8cb882f39f06590cbb9a52f21",
+			}, outTest{
+				payload: "a6694365727469666965645902bc2f15f386b80725ad11e1b2b55b53eeaea003a6cc6e5c359f344fb7af6f39dc043140578009262ea3247aef5e84c0a79e5962506185b54601b93d6d53bdc64dbcb27543657d2229096276a0e677f61cff7cb7c04bfc453b377bc780acb3163346cf3bcaffd0355ca29a6e47a981d85631b6c7c4bef767502d6982dc176df9776b217c2fe62886b139e48ea658c9aa450d995cc6d1bae5a220187694d120fa43fc77be3b3e57ea1a2d1f4247e6a580b8529063eb5dc44c5d64bab43474c66994bb36d149dfee4660ce5e1b8dc9edf8ca407a21519ecfdda6e7c3c3fd83257dc77e02f9c20cc1d68f56255a8e7147b3b6b334039d520215b5d219e899ebd454f3ba1f502f2b9710cc4cde4ec09d6ece17c19ee101eb42459cd3dfc1d7de76b58d4b0fed25335cb0756ea7eb2e762bc139ab8ec7ac8ea4240e034a95011649c5f856ed49e803f41e846a9043b320690b272d3b236af33e8a7d5fbb08a62edca052a58db32143c7b290a129303633ba944f5b9b66ce0123428bb20e7d8d26fb24bf0f5c2fa72a703d8bd31756ae540d887529832e48d8780efcab4e5c2cb59c89d853905562fd76920fd53f415e9cbdb6417a89de8b6ecf71051e877f2a230cee85b150f1479573b01ee7e486e2240b104df54f1dcfb469b946a6547eab1c32631d4171eb008d829231ce18155391570de540b1872a42c8547550f4b53a27430afa517784f32f849181243fc1ba3781caa4031829d491aa9b094ec6516c96880b013a25e4c94ad127452c0df9bd3ddb7eb108a7bfe65fee0626097c55c9a4ff55ac1a6ba5b85ae13408cb5d3e9a64bfbaa1848112f95ffc409229928ebcedde1ff4379ce69141d95dfc1fb10466d6fdfbeee2cccf961b71f2c59824ad43bf05ca9b0d5e48182f83cc61671354c72dfffb4a755cc44d3bd959078078a84ebfcf5a3817e820809ed87eb8d66adc5da1d55c33d8cb882f39f06590cbb9a52f216a45787069726174696f6e1b0000000468912f00674b65795479706567656432353531396a5369676e61747572657384a2684964656e746974795820a3bfdb632fdc227a276e17b2b573d31c7e6bbb236c25af5c5ffde17139d9e4bd675061796c6f61645840e48bab6c473adb8589ec43e49a9bee110eaf6abdbbbeda670ae4a6036e03167585def8dd209955c496980691e8b875ca9fe738958ffced2165b3caa776f7e303a2684964656e746974795820c57a0f83107b9ded621900615ca8ad5fda6f7bc839fb0fbf6f4b6af59611c710675061796c6f616458401fe3b0d50bfb7005fbc8b0342f78708d913d4d1e2cbd8e9733b4b84ba3bf63d240f7d10423daa68b6a054846a5170892bbc9bea2d29c3e3a2cbad183a661ef01a2684964656e746974795820d12f2a5061f93b378b94f72ddb2082e3e18681d902d4c65ae95c41001e8821be675061796c6f61645840b1983083e9819615b733ff866d7c73316949d28985b2b1b52d36225be06c94e2d8ffc42609ac8cbf87e4835ec9edc99c995559c4ff69faead8a4d2790727900ca2684964656e746974795820e896881e859d836443a4c6dfb7fbd3cd07160065b8eb000a4297c7c5c194e287675061796c6f61645840d180767a7424d2d1ea1d6868f48f009560bf529e86f7db83b7cade86241fef5831c4a8d65c0b010f58d177654bd3e97e153ca204953e9e6e99f6cab09c260109645479706569617574686f726974796756657273696f6e00",
+			},
+		},
+	}
+
+	for _, test := range certificateTests {
+		// expires 600 years after unix epoch
+		expiration := time.Unix(18934214400, 0).Unix()
+
+		sigKeys := []*eddsa.PrivateKey{}
+		for _, key := range test.in.signingKeys {
+			signingKeyRaw, err := hex.DecodeString(key)
+			assert.NoError(err)
+			signingKey := new(eddsa.PrivateKey)
+			signingKey.FromBytes(signingKeyRaw)
+			sigKeys = append(sigKeys, signingKey)
+		}
+
+		toSign, err := hex.DecodeString(test.in.toSign)
+		assert.NoError(err)
+		certificate, err := Sign(sigKeys[0], toSign, "authority", expiration)
+		assert.NoError(err)
+		for _, signingKey := range sigKeys[1:] {
+			certificate, err = SignMulti(signingKey, certificate)
+			assert.NoError(err)
+		}
+		payload, err := hex.DecodeString(test.want.payload)
+		assert.NoError(err)
+		assert.Equal(certificate, payload)
+	}
+}

katzenpost/katzenpost

crypto/cert/ed25519_vectors_test.go
@@ -1,0 +1,162 @@
+// ed25519_vectors_test.go - ed25519 certificate test vectors.
+// Copyright (C) 2018  David Stainton.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package cert
+
+import (
+	"encoding/hex"
+	"testing"
+	"time"
+
+	"github.com/katzenpost/core/crypto/eddsa"
+	"github.com/stretchr/testify/assert"
+)
+
+type inTest struct {
+	signingKey string
+	toSign     string
+}
+
+type outTest struct {
+	payload string
+}
+
+func TestEd25519SingleSignatureCertificateVectors(t *testing.T) {
+	assert := assert.New(t)
+
+	certificateTests := []struct {
+		in   inTest
+		want outTest
+	}{
+		{
+			inTest{
+				signingKey: "e818ee275ddb72b8e63758dfba3a90e0f5687dd59f28c2812e9861ba19fa33bffb731cf47b3732b24a5f9c00a0304b66d461b23e7292c5eb406ec09adc2d95e0",
+				toSign:     "32d4f52620a57aa2b02564c296c0b4b0dbeca5471704a9f40000706bcc134d2f",
+			}, outTest{
+				payload: "a569436572746966696564582032d4f52620a57aa2b02564c296c0b4b0dbeca5471704a9f40000706bcc134d2f6a45787069726174696f6e1b0000000468912f00674b65795479706567656432353531396a5369676e61747572657381a2684964656e746974795820fb731cf47b3732b24a5f9c00a0304b66d461b23e7292c5eb406ec09adc2d95e0675061796c6f6164584021fc981035cdd87fb480e5538d321276034c422c0adaf1413fe89a9c3ad99d5fdc18b8d7756ccc920ba4ad63a63ca4de5ffc3ab2ae9fcf028c01a56c025dcc066756657273696f6e00",
+			},
+		},
+		{
+			inTest{
+				signingKey: "f99bdb809088a609c270f612a0e44844d03d86a63e109966ca66f6678cc02a1065efbc72434d921af5285c0fd28af6ed8592c0ac44c834d9d98f5589ea21c03f",
+				toSign:     "ea7b8ef81c91986d93f0532c1de06ce25373c20782d8106ac6a4d85eee4802bb",
+			}, outTest{
+				payload: "a5694365727469666965645820ea7b8ef81c91986d93f0532c1de06ce25373c20782d8106ac6a4d85eee4802bb6a45787069726174696f6e1b0000000468912f00674b65795479706567656432353531396a5369676e61747572657381a2684964656e74697479582065efbc72434d921af5285c0fd28af6ed8592c0ac44c834d9d98f5589ea21c03f675061796c6f6164584027fffa55f9b2b8a0de9a7c7c3ceae28c5eeef46239ccac0ef840b425d5ee19ed826c152289294d5f63cd5090c53303bee9561fa98a1e74cc3c4cdd8cc67296066756657273696f6e00",
+			},
+		},
+		{
+			inTest{
+				signingKey: "13a40a6146651427a769e3dd17dc86d4497f5379aa077488fc8d40ce062675bf39b956e6aae2b76a13dd83c0eb96c3e2b22e6ad4e944625a7c0230b4561b82d9",
+				toSign:     "220b7f2efa95f089a75711aad6c843d9899af3e622f7adf06da6d7264ed0ece5093f423dc1fb5d1672643fa133680e49ce74ad5c0367ba0aee902b41a2ad3e9ffbb8db5503b33f0ae61ca28a01bf1b8615d8c2c5f9ac02572e89c76aef5c8b00c052ba4b13ef043fc9f39b90eed1e2e9bccc06a67d3cde025e0b7cfc15cc3efacc50b7bbc525e02f5c52f920b9e03bef07b320f37347359888feaed0ab4d83e0cc6877f00162b3ac424df7cee0b47f1a9f00d49651c2982e59504c889182cda7130e7f83949e193f",
+			}, outTest{
+				payload: "a56943657274696669656458c8220b7f2efa95f089a75711aad6c843d9899af3e622f7adf06da6d7264ed0ece5093f423dc1fb5d1672643fa133680e49ce74ad5c0367ba0aee902b41a2ad3e9ffbb8db5503b33f0ae61ca28a01bf1b8615d8c2c5f9ac02572e89c76aef5c8b00c052ba4b13ef043fc9f39b90eed1e2e9bccc06a67d3cde025e0b7cfc15cc3efacc50b7bbc525e02f5c52f920b9e03bef07b320f37347359888feaed0ab4d83e0cc6877f00162b3ac424df7cee0b47f1a9f00d49651c2982e59504c889182cda7130e7f83949e193f6a45787069726174696f6e1b0000000468912f00674b65795479706567656432353531396a5369676e61747572657381a2684964656e74697479582039b956e6aae2b76a13dd83c0eb96c3e2b22e6ad4e944625a7c0230b4561b82d9675061796c6f6164584004dcfeb5499b1c429209c815b49c78ca699f7e869c8cf0f8dff3e6475dd830a33afd72d1dc2d10f1eb198ba5e1e9f9d8efaead9c9f57b929ba3b6d1ee48f81026756657273696f6e00",
+			},
+		},
+	}
+
+	for _, test := range certificateTests {
+		// expires 600 years after unix epoch
+		expiration := time.Unix(18934214400, 0).Unix()
+		signingKeyRaw, err := hex.DecodeString(test.in.signingKey)
+		assert.NoError(err)
+		toSign, err := hex.DecodeString(test.in.toSign)
+		assert.NoError(err)
+		signingKey := new(eddsa.PrivateKey)
+		signingKey.FromBytes(signingKeyRaw)
+		certificate, err := Sign(signingKey, toSign, expiration)
+		assert.NoError(err)
+		payload, err := hex.DecodeString(test.want.payload)
+		assert.NoError(err)
+		assert.Equal(certificate, payload)
+	}
+}
+
+type multiSigTest struct {
+	signingKeys []string
+	toSign      string
+}
+
+func TestEd25519MultipleSignatureCertificateVectors(t *testing.T) {
+	assert := assert.New(t)
+
+	certificateTests := []struct {
+		in   multiSigTest
+		want outTest
+	}{
+		{
+			multiSigTest{
+				signingKeys: []string{
+					"e818ee275ddb72b8e63758dfba3a90e0f5687dd59f28c2812e9861ba19fa33bffb731cf47b3732b24a5f9c00a0304b66d461b23e7292c5eb406ec09adc2d95e0",
+					"f99bdb809088a609c270f612a0e44844d03d86a63e109966ca66f6678cc02a1065efbc72434d921af5285c0fd28af6ed8592c0ac44c834d9d98f5589ea21c03f",
+				},
+				toSign: "32d4f52620a57aa2b02564c296c0b4b0dbeca5471704a9f40000706bcc134d2f",
+			}, outTest{
+				payload: "a569436572746966696564582032d4f52620a57aa2b02564c296c0b4b0dbeca5471704a9f40000706bcc134d2f6a45787069726174696f6e1b0000000468912f00674b65795479706567656432353531396a5369676e61747572657382a2684964656e74697479582065efbc72434d921af5285c0fd28af6ed8592c0ac44c834d9d98f5589ea21c03f675061796c6f6164584059c6efc14d49f28b804b6fa3cb28e9066cb5b2ea5ddea3173284cf3cde7626e194022466258fc0c84f1006b05698d5932b24c4c50e91da91017ee48669c44c0fa2684964656e746974795820fb731cf47b3732b24a5f9c00a0304b66d461b23e7292c5eb406ec09adc2d95e0675061796c6f6164584021fc981035cdd87fb480e5538d321276034c422c0adaf1413fe89a9c3ad99d5fdc18b8d7756ccc920ba4ad63a63ca4de5ffc3ab2ae9fcf028c01a56c025dcc066756657273696f6e00",
+			},
+		},
+		{
+			multiSigTest{
+				signingKeys: []string{
+					"e818ee275ddb72b8e63758dfba3a90e0f5687dd59f28c2812e9861ba19fa33bffb731cf47b3732b24a5f9c00a0304b66d461b23e7292c5eb406ec09adc2d95e0",
+					"f99bdb809088a609c270f612a0e44844d03d86a63e109966ca66f6678cc02a1065efbc72434d921af5285c0fd28af6ed8592c0ac44c834d9d98f5589ea21c03f",
+					"13a40a6146651427a769e3dd17dc86d4497f5379aa077488fc8d40ce062675bf39b956e6aae2b76a13dd83c0eb96c3e2b22e6ad4e944625a7c0230b4561b82d9",
+				},
+				toSign: "32d4f52620a57aa2b02564c296c0b4b0dbeca5471704a9f40000706bcc134d2f",
+			}, outTest{
+				payload: "a569436572746966696564582032d4f52620a57aa2b02564c296c0b4b0dbeca5471704a9f40000706bcc134d2f6a45787069726174696f6e1b0000000468912f00674b65795479706567656432353531396a5369676e61747572657383a2684964656e74697479582039b956e6aae2b76a13dd83c0eb96c3e2b22e6ad4e944625a7c0230b4561b82d9675061796c6f61645840aabca1e5d737f49f37e3576d66eb7bf40e371cb411151a33fce57bad56aa8c7434d236eee23f2eb245a0d727ea1c4abd8a6a259ce01540a79af112e8ade10c04a2684964656e74697479582065efbc72434d921af5285c0fd28af6ed8592c0ac44c834d9d98f5589ea21c03f675061796c6f6164584059c6efc14d49f28b804b6fa3cb28e9066cb5b2ea5ddea3173284cf3cde7626e194022466258fc0c84f1006b05698d5932b24c4c50e91da91017ee48669c44c0fa2684964656e746974795820fb731cf47b3732b24a5f9c00a0304b66d461b23e7292c5eb406ec09adc2d95e0675061796c6f6164584021fc981035cdd87fb480e5538d321276034c422c0adaf1413fe89a9c3ad99d5fdc18b8d7756ccc920ba4ad63a63ca4de5ffc3ab2ae9fcf028c01a56c025dcc066756657273696f6e00",
+			},
+		},
+		{
+			multiSigTest{
+				signingKeys: []string{
+					"b82b2fbb1fa8fbf771dc85dc45e66a1626d44cf3361f3a61e387b266445346dec57a0f83107b9ded621900615ca8ad5fda6f7bc839fb0fbf6f4b6af59611c710",
+					"4b888f00728a6b7594f8eb6440e4a084889017d2d520ac7b3a8d0b5030872329d12f2a5061f93b378b94f72ddb2082e3e18681d902d4c65ae95c41001e8821be",
+					"e5ac1b84e158d06ace83947a5fba08af9b000d195503dae4d4a9728b5c0cef9ae896881e859d836443a4c6dfb7fbd3cd07160065b8eb000a4297c7c5c194e287",
+					"e66cc68c6606f1072485164e6ff989abaf1cef493ba25e922bbcb601769dc4dca3bfdb632fdc227a276e17b2b573d31c7e6bbb236c25af5c5ffde17139d9e4bd",
+				},
+				toSign: "2f15f386b80725ad11e1b2b55b53eeaea003a6cc6e5c359f344fb7af6f39dc043140578009262ea3247aef5e84c0a79e5962506185b54601b93d6d53bdc64dbcb27543657d2229096276a0e677f61cff7cb7c04bfc453b377bc780acb3163346cf3bcaffd0355ca29a6e47a981d85631b6c7c4bef767502d6982dc176df9776b217c2fe62886b139e48ea658c9aa450d995cc6d1bae5a220187694d120fa43fc77be3b3e57ea1a2d1f4247e6a580b8529063eb5dc44c5d64bab43474c66994bb36d149dfee4660ce5e1b8dc9edf8ca407a21519ecfdda6e7c3c3fd83257dc77e02f9c20cc1d68f56255a8e7147b3b6b334039d520215b5d219e899ebd454f3ba1f502f2b9710cc4cde4ec09d6ece17c19ee101eb42459cd3dfc1d7de76b58d4b0fed25335cb0756ea7eb2e762bc139ab8ec7ac8ea4240e034a95011649c5f856ed49e803f41e846a9043b320690b272d3b236af33e8a7d5fbb08a62edca052a58db32143c7b290a129303633ba944f5b9b66ce0123428bb20e7d8d26fb24bf0f5c2fa72a703d8bd31756ae540d887529832e48d8780efcab4e5c2cb59c89d853905562fd76920fd53f415e9cbdb6417a89de8b6ecf71051e877f2a230cee85b150f1479573b01ee7e486e2240b104df54f1dcfb469b946a6547eab1c32631d4171eb008d829231ce18155391570de540b1872a42c8547550f4b53a27430afa517784f32f849181243fc1ba3781caa4031829d491aa9b094ec6516c96880b013a25e4c94ad127452c0df9bd3ddb7eb108a7bfe65fee0626097c55c9a4ff55ac1a6ba5b85ae13408cb5d3e9a64bfbaa1848112f95ffc409229928ebcedde1ff4379ce69141d95dfc1fb10466d6fdfbeee2cccf961b71f2c59824ad43bf05ca9b0d5e48182f83cc61671354c72dfffb4a755cc44d3bd959078078a84ebfcf5a3817e820809ed87eb8d66adc5da1d55c33d8cb882f39f06590cbb9a52f21",
+			}, outTest{
+				payload: "a5694365727469666965645902bc2f15f386b80725ad11e1b2b55b53eeaea003a6cc6e5c359f344fb7af6f39dc043140578009262ea3247aef5e84c0a79e5962506185b54601b93d6d53bdc64dbcb27543657d2229096276a0e677f61cff7cb7c04bfc453b377bc780acb3163346cf3bcaffd0355ca29a6e47a981d85631b6c7c4bef767502d6982dc176df9776b217c2fe62886b139e48ea658c9aa450d995cc6d1bae5a220187694d120fa43fc77be3b3e57ea1a2d1f4247e6a580b8529063eb5dc44c5d64bab43474c66994bb36d149dfee4660ce5e1b8dc9edf8ca407a21519ecfdda6e7c3c3fd83257dc77e02f9c20cc1d68f56255a8e7147b3b6b334039d520215b5d219e899ebd454f3ba1f502f2b9710cc4cde4ec09d6ece17c19ee101eb42459cd3dfc1d7de76b58d4b0fed25335cb0756ea7eb2e762bc139ab8ec7ac8ea4240e034a95011649c5f856ed49e803f41e846a9043b320690b272d3b236af33e8a7d5fbb08a62edca052a58db32143c7b290a129303633ba944f5b9b66ce0123428bb20e7d8d26fb24bf0f5c2fa72a703d8bd31756ae540d887529832e48d8780efcab4e5c2cb59c89d853905562fd76920fd53f415e9cbdb6417a89de8b6ecf71051e877f2a230cee85b150f1479573b01ee7e486e2240b104df54f1dcfb469b946a6547eab1c32631d4171eb008d829231ce18155391570de540b1872a42c8547550f4b53a27430afa517784f32f849181243fc1ba3781caa4031829d491aa9b094ec6516c96880b013a25e4c94ad127452c0df9bd3ddb7eb108a7bfe65fee0626097c55c9a4ff55ac1a6ba5b85ae13408cb5d3e9a64bfbaa1848112f95ffc409229928ebcedde1ff4379ce69141d95dfc1fb10466d6fdfbeee2cccf961b71f2c59824ad43bf05ca9b0d5e48182f83cc61671354c72dfffb4a755cc44d3bd959078078a84ebfcf5a3817e820809ed87eb8d66adc5da1d55c33d8cb882f39f06590cbb9a52f216a45787069726174696f6e1b0000000468912f00674b65795479706567656432353531396a5369676e61747572657384a2684964656e746974795820a3bfdb632fdc227a276e17b2b573d31c7e6bbb236c25af5c5ffde17139d9e4bd675061796c6f61645840d47ca94d60175981d66f917f1f4bbe8fa42a13980bcf5a6d5259c010ce326ed12849c97386ec2971f5fc291f0b79b2a88b77e7d8190e4cd00b0a2121f866580ca2684964656e746974795820c57a0f83107b9ded621900615ca8ad5fda6f7bc839fb0fbf6f4b6af59611c710675061796c6f616458407256455cc9378b2e2fc5ba04840b39c493fb2a5eb58ac9dfe85b5c55f595497107fc57bef798fccdce69e699d288111d60a9e35521e852b3c3daf824b1dbb308a2684964656e746974795820d12f2a5061f93b378b94f72ddb2082e3e18681d902d4c65ae95c41001e8821be675061796c6f616458402982e2b91b14f7a5e9f6fe91474c6605eb827403bd2074463c0fb52aa0cbf3b2fd8dbc9d863a3c3bd56298152f1803292410fbdabac8713ee32777024637c609a2684964656e746974795820e896881e859d836443a4c6dfb7fbd3cd07160065b8eb000a4297c7c5c194e287675061796c6f616458409d9f1f59a1a0c9ae10bca5586aea5da615405abc7efa2c20498564bc7051399344e8e466b69f60292babe51fea6b43fb920c0c18d3535b8f64c587efc39f33076756657273696f6e00",
+			},
+		},
+	}
+
+	for _, test := range certificateTests {
+		// expires 600 years after unix epoch
+		expiration := time.Unix(18934214400, 0).Unix()
+
+		sigKeys := []*eddsa.PrivateKey{}
+		for _, key := range test.in.signingKeys {
+			signingKeyRaw, err := hex.DecodeString(key)
+			assert.NoError(err)
+			signingKey := new(eddsa.PrivateKey)
+			signingKey.FromBytes(signingKeyRaw)
+			sigKeys = append(sigKeys, signingKey)
+		}
+
+		toSign, err := hex.DecodeString(test.in.toSign)
+		assert.NoError(err)
+		certificate, err := Sign(sigKeys[0], toSign, expiration)
+		assert.NoError(err)
+		for _, signingKey := range sigKeys[1:] {
+			certificate, err = SignMulti(signingKey, certificate)
+			assert.NoError(err)
+		}
+		payload, err := hex.DecodeString(test.want.payload)
+		assert.NoError(err)
+		assert.Equal(certificate, payload)
+	}
+}