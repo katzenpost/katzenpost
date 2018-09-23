ff96578906565be8fb2cb8010996aa5b736a050c
David Stainton <dstainton415@gmail.com>
2018-09-23 20:35:40 +0100

    crypto/cert: fixed error return values

--- crypto/cert/cert.go
+++ crypto/cert/cert.go
@@ -32,7 +32,39 @@ const (
 	CertVersion = 0
 )
 
-var cborHandle *codec.CborHandle
+var (
+	cborHandle *codec.CborHandle
+
+	// ErrImpossibleDecode is an impossible decoding error.
+	ErrImpossibleDecode = errors.New("impossible to decode")
+
+	// ErrImpossibleEncode is an impossible encoding error.
+	ErrImpossibleEncode = errors.New("impossible to encode")
+
+	// ErrImpossibleOutOfMemory is an impossible out of memory error.
+	ErrImpossibleOutOfMemory = errors.New("impossible out of memory failure")
+
+	// ErrBadSignature indicates that the given signature does not sign the certificate.
+	ErrBadSignature = errors.New("signature does not sign certificate")
+
+	// ErrDuplicateSignature indicates that the given signature is already present in the certificate.
+	ErrDuplicateSignature = errors.New("signature must not be duplicate")
+
+	// ErrKeyTypeMismatch indicates that the given signer's key type is different than the signatures present already.
+	ErrKeyTypeMismatch = errors.New("certificate key type mismatch")
+
+	// ErrCertificateExpired indicates that the given certificate has expired.
+	ErrCertificateExpired = errors.New("certificate expired")
+
+	// ErrIdentitySignatureNotFound indicates that for the given signer identity there was no signature present in the certificate.
+	ErrIdentitySignatureNotFound = errors.New("failure to find signature associated with the given identity")
+
+	// ErrInvalidThreshold indicated the given threshold cannot be used.
+	ErrInvalidThreshold = errors.New("threshold must be equal or less than the number of verifiers")
+
+	// ErrThresholdNotMet indicates that there were not enough valid signatures to meet the threshold.
+	ErrThresholdNotMet = errors.New("threshold failure")
+)
 
 // Verifier is used to verify signatures.
 type Verifier interface {
@@ -88,18 +120,21 @@ func (c *certificate) message() ([]byte, error) {
 	message := new(bytes.Buffer)
 	err := binary.Write(message, binary.LittleEndian, c.Version)
 	if err != nil {
-		return nil, err
+		return nil, ErrImpossibleOutOfMemory
 	}
 	err = binary.Write(message, binary.LittleEndian, c.Expiration)
 	if err != nil {
-		return nil, err
+		return nil, ErrImpossibleOutOfMemory
 	}
 	_, err = message.Write([]byte(c.KeyType))
 	if err != nil {
-		return nil, err
+		return nil, ErrImpossibleOutOfMemory
 	}
 	_, err = message.Write([]byte(c.Certified))
-	return message.Bytes(), err
+	if err != nil {
+		return nil, ErrImpossibleOutOfMemory
+	}
+	return message.Bytes(), nil
 }
 
 // Sign uses the given Signer to create a certificate which
@@ -125,7 +160,7 @@ func Sign(signer Signer, data []byte, expiration int64) ([]byte, error) {
 	enc := codec.NewEncoderBytes(&out, cborHandle)
 	err = enc.Encode(&cert)
 	if err != nil {
-		return nil, err
+		return nil, ErrImpossibleEncode
 	}
 	return out, nil
 }
@@ -136,7 +171,7 @@ func GetCertified(rawCert []byte) ([]byte, error) {
 	dec := codec.NewDecoderBytes(rawCert, cborHandle)
 	err := dec.Decode(&cert)
 	if err != nil {
-		return nil, err
+		return nil, ErrImpossibleEncode
 	}
 	return cert.Certified, nil
 }
@@ -147,7 +182,7 @@ func GetSignatures(rawCert []byte) ([]Signature, error) {
 	dec := codec.NewDecoderBytes(rawCert, cborHandle)
 	err := dec.Decode(&cert)
 	if err != nil {
-		return nil, err
+		return nil, ErrImpossibleEncode
 	}
 	return cert.Signatures, nil
 }
@@ -159,14 +194,14 @@ func GetSignature(identity []byte, rawCert []byte) (*Signature, error) {
 	dec := codec.NewDecoderBytes(rawCert, cborHandle)
 	err := dec.Decode(&cert)
 	if err != nil {
-		return nil, err
+		return nil, ErrImpossibleDecode
 	}
 	for _, s := range cert.Signatures {
 		if bytes.Equal(identity, s.Identity) {
 			return &s, nil
 		}
 	}
-	return nil, errors.New("signature not found")
+	return nil, ErrIdentitySignatureNotFound
 }
 
 type byIdentity []Signature
@@ -191,10 +226,10 @@ func SignMulti(signer Signer, rawCert []byte) ([]byte, error) {
 	dec := codec.NewDecoderBytes(rawCert, cborHandle)
 	err := dec.Decode(cert)
 	if err != nil {
-		return nil, err
+		return nil, ErrImpossibleDecode
 	}
 	if signer.KeyType() != cert.KeyType {
-		return nil, errors.New("certificate key type mismatch")
+		return nil, ErrKeyTypeMismatch
 	}
 
 	// sign the certificate's message contents
@@ -210,7 +245,7 @@ func SignMulti(signer Signer, rawCert []byte) ([]byte, error) {
 	// dedup
 	for _, sig := range cert.Signatures {
 		if bytes.Equal(sig.Identity, signature.Identity) || bytes.Equal(sig.Payload, signature.Payload) {
-			return nil, errors.New("signature must not be duplicate")
+			return nil, ErrDuplicateSignature
 		}
 	}
 
@@ -222,7 +257,7 @@ func SignMulti(signer Signer, rawCert []byte) ([]byte, error) {
 	enc := codec.NewEncoderBytes(&out, cborHandle)
 	err = enc.Encode(&cert)
 	if err != nil {
-		return nil, err
+		return nil, ErrImpossibleEncode
 	}
 	return out, nil
 }
@@ -235,13 +270,13 @@ func AddSignature(verifier Verifier, signature Signature, rawCert []byte) ([]byt
 	dec := codec.NewDecoderBytes(rawCert, cborHandle)
 	err := dec.Decode(cert)
 	if err != nil {
-		return nil, err
+		return nil, ErrImpossibleDecode
 	}
 
 	// dedup
 	for _, sig := range cert.Signatures {
 		if bytes.Equal(sig.Identity, signature.Identity) || bytes.Equal(sig.Payload, signature.Payload) {
-			return nil, errors.New("signature must not be duplicate")
+			return nil, ErrDuplicateSignature
 		}
 	}
 
@@ -254,14 +289,14 @@ func AddSignature(verifier Verifier, signature Signature, rawCert []byte) ([]byt
 		cert.Signatures = append(cert.Signatures, signature)
 		sort.Sort(byIdentity(cert.Signatures))
 	} else {
-		return nil, errors.New("signature does not sign certificate")
+		return nil, ErrBadSignature
 	}
 	// serialize certificate
 	out := []byte{}
 	enc := codec.NewEncoderBytes(&out, cborHandle)
 	err = enc.Encode(&cert)
 	if err != nil {
-		return nil, err
+		return nil, ErrImpossibleEncode
 	}
 	return out, nil
 }
@@ -276,7 +311,7 @@ func Verify(verifier Verifier, rawCert []byte) ([]byte, error) {
 		return nil, err
 	}
 	if time.Unix(cert.Expiration, 0).Before(time.Now()) {
-		return nil, errors.New("certificate expired")
+		return nil, ErrCertificateExpired
 	}
 	for _, sig := range cert.Signatures {
 		if bytes.Equal(verifier.Identity(), sig.Identity) {
@@ -290,7 +325,7 @@ func Verify(verifier Verifier, rawCert []byte) ([]byte, error) {
 			return nil, nil
 		}
 	}
-	return nil, errors.New("failure to find signature associated with the given identity")
+	return nil, ErrIdentitySignatureNotFound
 }
 
 // VerifyAll returns the certified data if all of the given verifiers
@@ -313,7 +348,7 @@ func VerifyAll(verifiers []Verifier, rawCert []byte) ([]byte, error) {
 // error.
 func VerifyThreshold(verifiers []Verifier, threshold int, rawCert []byte) ([]byte, []Verifier, []Verifier, error) {
 	if threshold > len(verifiers) {
-		return nil, nil, nil, errors.New("threshold must be equal or less than the number of verifiers")
+		return nil, nil, nil, ErrInvalidThreshold
 	}
 	certified := []byte{}
 	count := 0
@@ -332,7 +367,7 @@ func VerifyThreshold(verifiers []Verifier, threshold int, rawCert []byte) ([]byt
 	if count >= threshold {
 		return certified, good, bad, nil
 	}
-	return nil, good, bad, errors.New("threshold failure")
+	return nil, good, bad, ErrThresholdNotMet
 }
 
 func init() {
