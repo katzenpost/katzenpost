3f60fb818c8e5dc7ecbf8e4385959af5440e598b
David Stainton <dstainton415@gmail.com>
2019-03-20 02:19:01 +0000

    cbor echo in rust: make it work

--- cbor_plugins/echo-rust/src/bin/echo_server.rs
+++ cbor_plugins/echo-rust/src/bin/echo_server.rs
@@ -1,4 +1,8 @@
 
+#[macro_use]
+extern crate log;
+extern crate log4rs;
+extern crate clap;
 extern crate rand;
 extern crate hyper;
 extern crate hyperlocal;
@@ -13,68 +17,143 @@ extern crate serde;
 #[macro_use]
 extern crate serde_cbor;
 
+extern crate serde_bytes;
+
+use std::path::Path;
+use std::str;
 use std::{fs, io};
 use std::collections::HashMap;
+use clap::{Arg, App};
+use log4rs::encode::pattern::PatternEncoder;
+use log4rs::config::{Appender, Config, Root};
+use log::LevelFilter;
 use futures::future;
+use futures::{Future, Stream};
 use rand::{thread_rng, Rng};
 use rand::distributions::Alphanumeric;
-use hyper::{header, Body, Method, StatusCode, Chunk};
+use hyper::{header, Method, StatusCode, Chunk};
 use hyper::service::service_fn;
+use hyper::Error;
 use hyper::body::Payload;
+use hyper::Body;
 use serde::{Deserialize, Serialize};
 use serde_cbor::from_slice;
 
+
 #[derive(Deserialize)]
+#[allow(non_snake_case)]
 pub struct Request {
     ID: u64,
+    #[serde(with = "serde_bytes")]
     Payload: Vec<u8>,
     HasSURB: bool,
 }
 
 #[derive(Serialize)]
+#[allow(non_snake_case)]
 pub struct Response {
+    #[serde(with = "serde_bytes")]
     Payload: Vec<u8>,
 }
 
 type Parameters = HashMap<String, String>;
 
-fn echo(http_request: hyper::Request<Body>) -> impl futures::Future<Item = hyper::Response<Body>, Error = io::Error> + Send {
-    let mut http_response = hyper::Response::new(Body::empty());
-    let mut cbor_response: Vec<u8> = Vec::new();
-    match (http_request.method(), http_request.uri().path()) {
-        (&Method::POST, "/request") => {
-            let mut reply = Response{
-                Payload: vec![],
-            };
+type BoxFut = Box<Future<Item = hyper::Response<hyper::Body>, Error = hyper::Error> + Send>;
 
-            let body = http_request.into_body();
-            body.fold();
-            //let req: Request = serde_cbor::from_slice().unwrap();
-            //let req: Request = serde_cbor::from_reader(body_stream).unwrap();
-            //let (parts, body) = http_request.into_parts();
-            //let fu = Box::new(body.concat2());
-
-            
-        },
+fn echo(req: hyper::Request<Body>) -> BoxFut {
+    let mut response = hyper::Response::new(Body::empty());
+    match (req.method(), req.uri().path()) {
         (&Method::POST, "/parameters") => {
-            // send an empty map
-            let mut parameters = Parameters::new();
-            cbor_response = serde_cbor::to_vec(&parameters).unwrap();
-        },
-        _ => {
-            *http_response.status_mut() = StatusCode::NOT_FOUND;
+            let params = Parameters::new();
+            let cbor_params = serde_cbor::to_vec(&params).unwrap();
+            *response.body_mut() = Body::from(cbor_params);
         }
-    }
-    futures::future::ok(
-        hyper::Response::builder()
-            .header(header::CONTENT_TYPE, "application/octet-stream")
-            .header(header::CONTENT_LENGTH, cbor_response.len())
-            .body(hyper::Body::from(cbor_response))
-            .expect("failed to create response")
-   ) 
+
+        (&Method::POST, "/request") => {
+            let _response = req.into_body().concat2().map(move |chunk| {
+                let body = chunk.iter().cloned().collect::<Vec<u8>>();
+                let body_result: Result<Request, serde_cbor::error::Error> = serde_cbor::from_slice(&body.to_vec());
+                match body_result {
+                    Ok(request) =>{
+                        let inner_response = Response {
+                            Payload: request.Payload,
+                        };
+                        let cbor_response_result = serde_cbor::to_vec(&inner_response);
+                        match cbor_response_result {
+                            Ok(cbor_response) => {
+                                *response.body_mut() = Body::from(cbor_response);
+                                return response;
+                            },
+                            Err(_e) => {
+                                info!("FAILED to serialize CBOR response: {}", _e);
+                                return response;
+                            },
+                        }
+                    },
+                    Err(_e) => {
+                        info!("FAILED to deserialize CBOR request: {}", _e);
+                        return response;
+                    },
+                }
+            });
+            return Box::new(_response);
+        }
+
+        // The 404 Not Found route...
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        }
+    };
+
+    Box::new(future::ok(response))
+}
+
+fn init_logger(log_dir: &str) {
+    use log4rs::append::file::FileAppender;
+
+    let rand_string: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(10)
+        .collect();
+    let log_path = Path::new(log_dir).join(format!("echo_rust_{}.log", rand_string));
+
+    let requests = FileAppender::builder()
+        .encoder(Box::new(PatternEncoder::new("{d} - {m}{n}")))
+        .build(log_path)
+        .unwrap();
+
+    let config = Config::builder()
+        .appender(Appender::builder().build("requests", Box::new(requests)))
+        .build(Root::builder().appender("requests").build(LevelFilter::Info))
+        .unwrap();
+    let _handle = log4rs::init_config(config).unwrap();
 }
 
 fn run() -> io::Result<()> {
+    let matches = App::new("Katzenpost Echo Service written in Rust")
+        .version("1.0")
+        .author("David Stainton <dawuud@riseup.net>")
+        .about("Functions as a plugin to be executed by the Katzenpost server.")
+        .arg(Arg::with_name("log_dir")
+             .short("l")
+             .long("log_dir")
+             .required(true)
+             .value_name("DIR")
+             .help("Sets the log directory.")
+             .takes_value(true))
+        .get_matches();
+    let log_dir = matches.value_of("log_dir").unwrap();
+
+    // Ensure log_dir exists and is a directory.
+    if !Path::new(log_dir).is_dir() {
+        panic!("log_dir must exist and be a directory");
+    }
+
+    // Setup logging.
+    init_logger(log_dir);
+
+    info!("starting Katzenpost Echo Service");
+    // Start our service
     let rand_string: String = thread_rng()
         .sample_iter(&Alphanumeric)
         .take(10)
@@ -88,6 +167,7 @@ fn run() -> io::Result<()> {
     let svr = hyperlocal::server::Server::bind(&socket_path, || service_fn(echo))?;
     println!("{}\n", socket_path);
     svr.run()?;
+    info!("shutting down Katzenpost Echo Service...");
     Ok(())
 }
 
