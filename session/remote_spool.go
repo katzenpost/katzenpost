890448d7027235a59a0f16e87ec6c7b129151273
David Stainton <dstainton415@gmail.com>
2019-03-22 12:22:11 +0000

    work-in-progress multispool client api

---
+++ session/remote_spool.go
@@ -0,0 +1,34 @@
+// remote_spool.go - client session remote spool operations
+// Copyright (C) 2019  David Stainton.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package session
+
+import (
+	"github.com/katzenpost/client/multispool"
+	"github.com/katzenpost/core/crypto/eddsa"
+)
+
+func (s *Session) SendCreateSpool(privKey *eddsa.PrivateKey, recipient, provider string) error {
+	cmd, err := multispool.CreateSpool(privKey)
+	if err != nil {
+		return err
+	}
+	msgID, err := SendUnreliableQuery(recipient, provider, cmd)
+	if err != nil {
+		return err
+	}
+	return nil
+}
