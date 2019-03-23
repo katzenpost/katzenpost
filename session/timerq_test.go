cce1e14b8997efbe9e8168cf856c08f0f43dfde4
Masala <masala@riseup.net>
2019-03-23 10:25:16 -0400

    Adds TimerQ type and unit tests
    The TimerQ is a container that holds messages until an expiry time, upon
    which it will pass the message via an interface that has one method,
    Push.
    
    Each message is placed into a priority queue ordered by earliest
    message expiry. A single time.Timer is used to wake the worker thread
    for the message expiry events. The worker thread is also woken upon
    Enqueue and Remove method calls in order to update the timer.
    
    TODO: the message expiry function should be corrected/improved. Possibly
    it should be passed as a constructor argument.

---
+++ session/timerq_test.go
@@ -0,0 +1,115 @@
+// timerq_test.go - Time delayed queue tests
+// Copyright (C) 2018  Masala, David Stainton.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package session
+
+import (
+	"io"
+	"testing"
+	"time"
+
+	"github.com/katzenpost/core/crypto/rand"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewTimerQ(t *testing.T) {
+	// create a Queue for rescheduled messages
+	q := new(Queue)
+
+	a := NewTimerQ(q)
+	a.Halt()
+}
+
+func TestTimerQPush(t *testing.T) {
+	assert := assert.New(t)
+
+	// create a queue for rescheduled messages
+	q := new(Queue)
+
+	a := NewTimerQ(q)
+
+	// enqueue 10 messages
+	for i := 0; i < 10; i++ {
+		m := &Message{}
+		m.ID = new([16]byte)
+
+		m.SentAt = time.Now()
+		m.ReplyETA = 200 * time.Millisecond
+		io.ReadFull(rand.Reader, m.ID[:])
+		a.Push(m)
+		<-time.After(1 * time.Millisecond)
+	}
+	t.Logf("Sent 10 messages")
+
+	// wait for all of the timers to expire and each message to be enqueued in q
+	<-time.After(1 * time.Second)
+
+	j := 0
+	for {
+		_, err := q.Pop()
+		if err == ErrQueueEmpty {
+			break
+		}
+		j++
+	}
+	t.Logf("Pop() %d messages", j)
+
+	// Verify that all messages were placed into q
+	assert.Equal(10, j)
+	a.Halt()
+}
+
+func TestTimerQRemove(t *testing.T) {
+	assert := assert.New(t)
+
+	// create a Queue for forwarded messages
+	q := new(Queue)
+
+	a := NewTimerQ(q)
+
+	// enqueue 10 messages, and call TimerQ.Remove() on half of them before their timers expire
+	for i := 0; i < 10; i++ {
+		m := &Message{}
+		m.ID = new([16]byte)
+
+		m.SentAt = time.Now()
+		m.ReplyETA = 100 * time.Millisecond
+		io.ReadFull(rand.Reader, m.ID[:])
+		a.Push(m)
+		<-time.After(20 * time.Millisecond)
+		if i%2 == 0 {
+			er := a.Remove(m)
+			assert.NoError(er)
+		}
+		<-time.After(80 * time.Millisecond)
+	}
+	t.Logf("Sent 10 messages")
+	<-time.After(2 * time.Second)
+
+	j := 0
+	for {
+		_, err := q.Pop()
+		if err == ErrQueueEmpty {
+			break
+		}
+		j++
+	}
+	t.Logf("Popped %d messages", j)
+
+	// verify that half of the messages were sent to q
+	assert.Equal(5, j)
+	a.Halt()
+}
