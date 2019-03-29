0429120fa68f85e905a4bcb371978b3f18fc74f6
David Stainton <dstainton415@gmail.com>
2019-03-29 04:42:47 +0000

    Add multispool client commands

--- session/remote_spool.go
+++ session/remote_spool.go
@@ -17,19 +17,70 @@
 package session
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/katzenpost/client/multispool"
 	"github.com/katzenpost/core/crypto/eddsa"
 )
 
+const OKStatus = "OK"
+
+func (s *Session) submitCommand(cmd []byte, recipient, provider string) ([]byte, error) {
+	id, err := s.SendUnreliableMessage(recipient, provider, cmd)
+	if err != nil {
+		return nil, err
+	}
+	reply, err := s.WaitForReply(id)
+	if err != nil {
+		return nil, err
+	}
+	spoolResponse, err := multispool.SpoolResponseFromBytes(reply)
+	if err != nil {
+		return nil, err
+	}
+	if strings.Compare(spoolResponse.Status, OKStatus) == 0 {
+		return spoolResponse.Message, nil
+	}
+	return nil, fmt.Errorf("spool command failure: %s", spoolResponse.Status)
+}
+
 // SendCreateSpool is a work-in-progress right now.
-func (s *Session) SendCreateSpool(privKey *eddsa.PrivateKey, recipient, provider string) error {
+func (s *Session) CreateSpool(privKey *eddsa.PrivateKey, recipient, provider string) error {
 	cmd, err := multispool.CreateSpool(privKey)
 	if err != nil {
 		return err
 	}
-	_, err = s.SendUnreliableMessage(recipient, provider, cmd)
+	_, err = s.submitCommand(cmd, recipient, provider)
+	return err
+}
+
+func (s *Session) PurgeSpool(spoolID [multispool.SpoolIDSize]byte, privKey *eddsa.PrivateKey, recipient, provider string) error {
+	cmd, err := multispool.PurgeSpool(spoolID, privKey)
 	if err != nil {
 		return err
 	}
-	return nil
+	_, err = s.submitCommand(cmd, recipient, provider)
+	return err
+}
+
+func (s *Session) AppendToSpool(spoolID [multispool.SpoolIDSize]byte, message []byte, recipient, provider string) error {
+	cmd, err := multispool.AppendToSpool(spoolID, message)
+	if err != nil {
+		return err
+	}
+	_, err = s.submitCommand(cmd, recipient, provider)
+	return err
+}
+
+func (s *Session) ReadFromSpool(spoolID [multispool.SpoolIDSize]byte, messageID [multispool.MessageIDSize]byte,
+	privKey *eddsa.PrivateKey,
+	recipient,
+	provider string) ([]byte, error) {
+
+	cmd, err := multispool.ReadFromSpool(spoolID, messageID, privKey)
+	if err != nil {
+		return nil, err
+	}
+	return s.submitCommand(cmd, recipient, provider)
 }
