03346ef0d72e136ba378e05405556263b2fffc2e
David Stainton <dstainton415@gmail.com>
2019-04-08 23:21:15 +0000

    Add logging to file via cli

--- main.go
+++ main.go
@@ -17,19 +17,56 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"os"
+	"path"
 
 	"github.com/katzenpost/client/multispool"
 	"github.com/katzenpost/server/cborplugin"
+	"github.com/op/go-logging"
 	"github.com/ugorji/go/codec"
 )
 
 var cborHandle = new(codec.CborHandle)
+var log = logging.MustGetLogger("memspool")
+var logFormat = logging.MustStringFormatter(
+	"%{level:.4s} %{id:03x} %{message}",
+)
+
+func stringToLogLevel(level string) (logging.Level, error) {
+	switch level {
+	case "DEBUG":
+		return logging.DEBUG, nil
+	case "INFO":
+		return logging.INFO, nil
+	case "NOTICE":
+		return logging.NOTICE, nil
+	case "WARNING":
+		return logging.WARNING, nil
+	case "ERROR":
+		return logging.ERROR, nil
+	case "CRITICAL":
+		return logging.CRITICAL, nil
+	}
+	return -1, fmt.Errorf("invalid logging level %s", level)
+}
+
+func setupLoggerBackend(level logging.Level, writer io.Writer) logging.LeveledBackend {
+	format := logFormat
+	backend := logging.NewLogBackend(writer, "", 0)
+	formatter := logging.NewBackendFormatter(backend, format)
+	leveler := logging.AddModuleLevel(formatter)
+	leveler.SetLevel(level, "memspool")
+	return leveler
+}
 
 func parametersHandler(response http.ResponseWriter, req *http.Request) {
+	log.Debug("parametersHandler")
+
 	params := new(cborplugin.Parameters)
 	var serialized []byte
 	enc := codec.NewEncoderBytes(&serialized, new(codec.CborHandle))
@@ -43,6 +80,8 @@ func parametersHandler(response http.ResponseWriter, req *http.Request) {
 }
 
 func requestHandler(spoolMap *MemSpoolMap, response http.ResponseWriter, request *http.Request) {
+	log.Debug("requestHandler")
+
 	req := cborplugin.Request{
 		Payload: make([]byte, 0),
 	}
@@ -73,11 +112,41 @@ func requestHandler(spoolMap *MemSpoolMap, response http.ResponseWriter, request
 	if err != nil {
 		panic(err)
 	}
+	log.Debug("success")
 }
 
 func main() {
+	var logLevel string
+	var logDir string
+	flag.StringVar(&logDir, "log_dir", "", "logging directory")
+	flag.StringVar(&logLevel, "log_level", "DEBUG", "logging level could be set to: DEBUG, INFO, NOTICE, WARNING, ERROR, CRITICAL")
+	flag.Parse()
+
+	level, err := stringToLogLevel(logLevel)
+	if err != nil {
+		fmt.Println("Invalid logging-level specified.")
+		os.Exit(1)
+	}
+
+	// Ensure that the log directory exists.
+	s, err := os.Stat(logDir)
+	if os.IsNotExist(err) {
+		fmt.Printf("Log directory '%s' doesn't exist.", logDir)
+		os.Exit(1)
+	}
+	if !s.IsDir() {
+		fmt.Println("Log directory must actually be a directory.")
+		os.Exit(1)
+	}
+
+	// Log to a file.
+	logFile := path.Join(logDir, fmt.Sprintf("memspool.%d.log", os.Getpid()))
+	f, err := os.Create(logFile)
+	logBackend := setupLoggerBackend(level, f)
+	log.SetBackend(logBackend)
+
 	server := http.Server{}
-	socketFile := fmt.Sprintf("/tmp/%d.echo.socket", os.Getpid())
+	socketFile := fmt.Sprintf("/tmp/%d.memspool.socket", os.Getpid())
 
 	unixListener, err := net.Listen("unix", socketFile)
 	if err != nil {
@@ -89,7 +158,10 @@ func main() {
 	}
 	http.HandleFunc("/request", _requestHandler)
 	http.HandleFunc("/parameters", parametersHandler)
+
 	fmt.Printf("%s\n", socketFile)
+	log.Debug("memspool server started.")
+
 	server.Serve(unixListener)
 	os.Remove(socketFile)
 }
