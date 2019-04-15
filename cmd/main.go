6b612aaaae2ffb1aa276c8a6ec584099651adb94
David Stainton <dstainton415@gmail.com>
2019-04-15 23:53:33 +0000

    Add example interactive prompt

---
+++ cmd/main.go
@@ -0,0 +1,37 @@
+// main.go - main function of client
+// Copyright (C) 2019  David Stainton.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"fmt"
+	"github.com/c-bata/go-prompt"
+)
+
+func completer(d prompt.Document) []prompt.Suggest {
+	s := []prompt.Suggest{
+		{Text: "send", Description: "Send a message."},
+		{Text: "inbox", Description: "Read inbox messages."},
+		{Text: "add_contact", Description: "Add a new contact."},
+	}
+	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
+}
+
+func main() {
+	fmt.Println("Enter a command.")
+	t := prompt.Input("> ", completer)
+	fmt.Println("You selected " + t)
+}
