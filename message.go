f579aea22634f42a8791e3caaaca3763dfe37133
David Stainton <dstainton415@gmail.com>
2019-10-19 07:33:46 +0000

    Add SentMessageDescriptor type

---
+++ message.go
@@ -0,0 +1,25 @@
+// message.go - client message descriptor types
+// Copyright (C) 2019  David Stainton.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package catshadow
+
+type SentMessageDescriptor struct {
+	// Nickname is the contact nickname to whom a message was sent.
+	Nickname string
+
+	// ConversationIndex is the numerical index for the given message in the conversation.
+	ConversationIndex int
+}
