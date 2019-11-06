9e12664c1d72331720560733420a1488a9a77a01
David Stainton <dstainton415@gmail.com>
2019-11-06 09:33:25 +0000

    fix config file

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 // main.go - Katzenpost ping tool
-// Copyright (C) 2018  David Stainton
+// Copyright (C) 2018, 2019  David Stainton
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
