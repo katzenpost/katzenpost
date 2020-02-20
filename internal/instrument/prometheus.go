a8c1f86d8acadd67325a8eada83f9d6b1208c89b
Mikerah <mikerahqc@protonmail.com>
2020-02-20 10:54:28 -0500

    Renamed metrics according to prometheus naming conventions

--- internal/instrument/prometheus.go
+++ internal/instrument/prometheus.go
@@ -15,14 +15,14 @@ import (
 var (
 	incomingConns = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "katzenpost_incoming_total_requests",
+			Name: "katzenpost_incoming_requests_total",
 			Help: "Number of incoming requests",
 		},
 		[]string{"command"},
 	)
 	outgoingConns = prometheus.NewCounter(
 		prometheus.CounterOpts{
-			Name: "katzenpost_outgoing_total_connections",
+			Name: "katzenpost_outgoing_connections_total",
 			Help: "Number of outgoing connections",
 		},
 	)
@@ -34,49 +34,49 @@ var (
 	)
 	packetsDropped = prometheus.NewCounter(
 		prometheus.CounterOpts{
-			Name: "katzenpost_number_of_dropped_packets",
+			Name: "katzenpost_dropped_packets_total",
 			Help: "Number of dropped packets",
 		},
 	)
 	packetsReplayed = prometheus.NewCounter(
 		prometheus.CounterOpts{
-			Name: "katzenpost_number_of_replayed_packets",
+			Name: "katzenpost_replayed_packets_total",
 			Help: "Number of replayed packets"
 		},
 	)
 	ignoredPKIDocs = prometheus.NewCounter(
 		prometheus.CounterOpts{
-			Name: "katzenpost_total_documents_ignored",
+			Name: "katzenpost_documents_ignored_total",
 			Help: "Number of ignored PKI Documents"
 		},
 	)
 	kaetzchenRequests = prometheus.NewCounter(
 		prometheus.CounterOpts{
-			Name: "katzenpost_kaetzchen_total_requests",
+			Name: "katzenpost_kaetzchen_requests_total",
 			Help: "Number of Kaetzchen requests",
 		},
 	)
 	kaetzchenPacketsDropped = prometheus.NewCounter(
 		prometheus.CounterOpts{
-			Name: "katzenpost_kaetzchen_number_of_dropped_packets",
+			Name: "katzenpost_kaetzchen_dropped_packets_total",
 			Help: "Number of dropped kaetzchen packets",
 		},
 	)
 	kaetzchenRequestsDropped = prometheus.NewCounter(
 		prometheus.CounterOpts{
-			Name: "katzenpost_kaetzchen_total_dropped_requests"
+			Name: "katzenpost_kaetzchen_dropped_requests_total"
 			Help: "Number of total dropped kaetzchen requests",
 		},
 	)
 	kaetzchenRequestsFailed = prometheus.NewCounter(
 		prometheus.CounterOpts{
-			Name: "katzenpost_kaetzchen_total_failed_requests",
+			Name: "katzenpost_kaetzchen_failed_requests_total",
 			Help: "Number of total failed kaetzchen requests",
 		},
 	)
 	mixPacketsDropped = prometheus.NewCounter(
 		prometheus.CounterOpts{
-			Name: "katzenpost_kaetzchen_number_of_mix_packets_dropped",
+			Name: "katzenpost_kaetzchen_mix_packets_dropped_total",
 			Help: "Number of total dropped mixed packets",
 		},
 	)
@@ -88,40 +88,40 @@ var (
 	)
 	pkiDocs = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "katzenpost_total_number_of_pki_docs_per_epoch"
+			Name: "katzenpost_pki_docs_per_epoch_total"
 			Help: "Number of pki docs in an epoch"
 		},
 		[]string{"epoch"},
 	)
 	cancelledOutgoingConns = prometheus.NewCounter(
 		prometheus.CounterOpts{
-			Name: "katzenpost_total_cancelled_outgoing_connections",
+			Name: "katzenpost_cancelled_outgoing_connections_total",
 			Help: "Number of cancelled outgoing connections",
 		},
 	)
 	fetchedPKIDocs = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "katzenpost_total_fetched_pki_docs_per_epoch",
+			Name: "katzenpost_fetched_pki_docs_per_epoch_total",
 			Help: "Number of fetch PKI docs per epoch",
 		},
 		[]string{"epoch"},
 	)
 	failedFetchPKIDocs = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "katzenpost_total_failed_fetch_pki_docs_per_epoch",
+			Name: "katzenpost_failed_fetch_pki_docs_per_epoch_total",
 			Help: "Number of failed PKI docs fetches per epoch"
 		}
 	)
 	failedPKICacheGeneration = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "katzenpost_total_failed_pki_cache_generation_per_epoch",
+			Name: "katzenpost_failed_pki_cache_generation_per_epoch_total",
 			Help: "Number of failed PKI caches generation per epoch",
 		},
 		[]string{"epoch"},
 	)
 	invalidPKICache = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name : "katzenpost_total_invalid_pki_cache_per_epoch",
+			Name : "katzenpost_invalid_pki_cache_per_epoch_total",
 			Help : "Number of invalid PKI caches per epoch"
 		}
 	)
@@ -146,7 +146,27 @@ func Init() {
 	prometheus.MustRegister(cancelledOutgoingConns)
 	prometheus.MustRegister(fetchedPKIDocs)
 	prometheus.MustRegister(failedFetchPKIDocs)
-	prometheus.MustRegister(failedPKICacheGeneration)
+	prometheus.MustRegister(fErica proves that she is salted peer that Bob is connected to.
+		Erica proves she ran the SMP correctly for each peer.
+		Alice completes SMP correctly for the search term Carroll.
+		Alice aggregates the proofs together to create a single proof of connection that hides the SMP which could allow Erica to identify the proof.
+		￼
+		1058×794 37 KB
+		Who knows what
+		Alice knows that she is connected by two hops to Carroll.
+		Bob knows that one of his peers was trying to search for someone
+		Erica knows that one of her peer was searching for someone
+		In order to prevent people from seeing if a search attempt succeeded its important to create proofs for every search attempt.
+		
+		We also need to continue the search to a certain depth in the social tree even if we have created the proof.
+		
+		This might be prohibitively expensive but we can make a trade off here.
+		
+		Attacks
+		An attacker can brute force the network looking for peers
+		We can use a ZKP in order to rate limit all requests. github.com/kobigurk/semaphore, Semaphore RLN, rate limiting nullifier for spam prevention in anonymous p2p setting 1
+		
+		Loop attack: An attacker creates a loop of peailedPKICacheGeneration)
 	prometheus.MustRegister(invalidPKICache)
 
 	// Expose registered metrics via HTTP
