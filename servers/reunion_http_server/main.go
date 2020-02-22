85e3f4582004b266eccf365507458f50edf8a990
David Stainton <dstainton415@gmail.com>
2020-02-22 18:37:41 +0000

    Add simple http server and tests

---
+++ servers/reunion_http_server/main.go
@@ -0,0 +1,91 @@
+// main.go - Reunion http server.
+// Copyright (C) 2020  David Stainton.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/katzenpost/core/log"
+	"github.com/katzenpost/reunion/commands"
+	"github.com/katzenpost/reunion/server"
+	"gopkg.in/op/go-logging.v1"
+)
+
+func httpReunionServerFactory(s *server.Server, log *logging.Logger) func(w http.ResponseWriter, req *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
+
+		rawRequest, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			log.Errorf("reunion HTTP server failed to ReadAll raw command data: %s", err.Error())
+			return
+		}
+		log.Debugf("raw request size is %d", len(rawRequest))
+		if len(rawRequest) == 0 {
+			log.Error("error read zero sized request body from client")
+			return
+		}
+		cmd, err := commands.FromBytes(rawRequest)
+		if err != nil {
+			log.Errorf("1 reunion HTTP server invalid query command: %s", err.Error())
+			return
+		}
+		haltCh := make(chan interface{})
+		replyCmd, err := s.ProcessQuery(cmd, haltCh)
+		if err != nil {
+			log.Errorf("reunion HTTP server invalid reply command: %s", err.Error())
+			return
+		}
+		rawReply := replyCmd.ToBytes()
+		_, err = w.Write(rawReply)
+		if err != nil {
+			log.Errorf("reunion HTTP server failure to send reply command: %s", err.Error())
+			return
+		}
+	}
+}
+
+func runHttpServer(address, urlPath, logPath, logLevel string) *http.Server {
+	logBackend, err := log.New(logPath, logLevel, false)
+	if err != nil {
+		panic(err)
+	}
+	reunionServer := server.NewServer()
+	httpServeMux := http.NewServeMux()
+	httpLog := logBackend.GetLogger("reunion_http_server")
+	httpServeMux.HandleFunc(urlPath, httpReunionServerFactory(reunionServer, httpLog))
+	httpServer := &http.Server{
+		Addr:           address,
+		Handler:        httpServeMux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	go httpServer.ListenAndServe()
+	return httpServer
+}
+
+func main() {
+	address := flag.String("l", "127.0.0.1:12345", "Listen address. Defaults to 127.0.0.1:12345")
+	urlPath := flag.String("p", "/reunion", "Reunion URL path. Defaults to /reunion")
+	logPath := flag.String("log", "", "Log file path. Defaults to STDOUT")
+	logLevel := flag.String("level", "DEBUG", "Log level. Defaults to DEBUG")
+	flag.Parse()
+	runHttpServer(*address, *urlPath, *logPath, *logLevel)
+}
---
+++ servers/reunion_http_server/server_test.go
@@ -0,0 +1,167 @@
+// main.go - Reunion http server.
+// Copyright (C) 2020  David Stainton.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"context"
+	"crypto/sha256"
+	"fmt"
+	"sync"
+	"testing"
+
+	"github.com/katzenpost/core/log"
+	"github.com/katzenpost/reunion/client"
+	"github.com/katzenpost/reunion/commands"
+	"github.com/katzenpost/reunion/crypto"
+	"github.com/katzenpost/reunion/transports/http"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHTTPServer1(t *testing.T) {
+	require := require.New(t)
+
+	address := "127.0.0.1:12345"
+	urlPath := "/reunion"
+	logPath := ""
+	logLevel := "DEBUG"
+	reunionServer := runHttpServer(address, urlPath, logPath, logLevel)
+
+	epoch := uint64(1234)
+	url := fmt.Sprintf("http://%s%s", address, urlPath)
+	httpTransport := http.NewHTTPTransport(url)
+
+	myT1 := make([]byte, crypto.Type1MessageSize)
+	blurb := []byte("cats and honeybadgers use cryptography for the win twice on sunday")
+	copy(myT1[:len(blurb)], blurb)
+	h := sha256.New()
+	h.Write(myT1)
+	myT1Hash := h.Sum(nil)
+	myT1HashAr := [sha256.Size]byte{}
+	copy(myT1HashAr[:], myT1Hash)
+
+	sendT1Cmd := &commands.SendT1{
+		Epoch:   epoch,
+		Payload: myT1,
+	}
+	rawRequest := sendT1Cmd.ToBytes()
+	sendT1Cmd2, err := commands.FromBytes(rawRequest)
+	require.NoError(err)
+	rawRequest2 := sendT1Cmd2.ToBytes()
+	require.Equal(rawRequest, rawRequest2)
+
+	haltCh := make(chan interface{})
+	serverReplyRaw, err := httpTransport.Query(sendT1Cmd, haltCh)
+	require.NoError(err)
+	require.NotNil(serverReplyRaw)
+
+	response, ok := serverReplyRaw.(*commands.MessageResponse)
+	require.True(ok)
+	require.Equal(response.ErrorCode, uint8(commands.ResponseStatusOK))
+
+	sendFetch := &commands.FetchState{
+		Epoch:  epoch,
+		T1Hash: myT1HashAr,
+	}
+
+	serverReplyRaw, err = httpTransport.Query(sendFetch, haltCh)
+	require.NoError(err)
+	require.NotNil(serverReplyRaw)
+
+	reunionServer.Shutdown(context.TODO())
+}
+
+func TestHTTPServer2(t *testing.T) {
+	require := require.New(t)
+
+	address := "127.0.0.1:12345"
+	urlPath := "/reunion"
+	logPath := ""
+	logLevel := "DEBUG"
+	reunionServer := runHttpServer(address, urlPath, logPath, logLevel)
+
+	epoch := uint64(1234)
+	url := fmt.Sprintf("http://%s%s", address, urlPath)
+	httpTransport := http.NewHTTPTransport(url)
+
+	// variable shared among reunion clients
+	f := ""
+	level := "DEBUG"
+	disable := false
+	logBackend, err := log.New(f, level, disable)
+	require.NoError(err)
+
+	srv := []byte{1, 2, 3}
+	passphrase := []byte("blah blah motorcycle pencil sharpening gas tank")
+
+	var bobResult []byte
+	var aliceResult []byte
+
+	// alice client
+	alicePayload := []byte("Hello Bobby, what's up dude?")
+	aliceContactID := uint64(1)
+	require.NoError(err)
+	aliceExchangelog := logBackend.GetLogger("alice_exchange")
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	aliceUpdateCh := make(chan client.ReunionUpdate)
+	go func() {
+		for {
+			update := <-aliceUpdateCh
+			if len(update.Result) > 0 {
+				aliceResult = update.Result
+				fmt.Printf("\n Alice got result: %s\n\n", update.Result)
+				wg.Done()
+			}
+		}
+	}()
+
+	aliceExchange, err := client.NewExchange(alicePayload, aliceExchangelog, httpTransport, aliceContactID, passphrase, srv, epoch, aliceUpdateCh)
+	require.NoError(err)
+
+	// bob client
+	bobPayload := []byte("Hello Alice, what's cracking?")
+	bobContactID := uint64(2)
+	require.NoError(err)
+	bobExchangelog := logBackend.GetLogger("bob_exchange")
+
+	wg.Add(1)
+	bobUpdateCh := make(chan client.ReunionUpdate)
+	go func() {
+		for {
+			update := <-bobUpdateCh
+			if len(update.Result) > 0 {
+				bobResult = update.Result
+				fmt.Printf("\n Bob got result: %s\n\n", update.Result)
+				wg.Done()
+			}
+		}
+	}()
+
+	bobExchange, err := client.NewExchange(bobPayload, bobExchangelog, httpTransport, bobContactID, passphrase, srv, epoch, bobUpdateCh)
+	require.NoError(err)
+
+	go aliceExchange.Run()
+	go bobExchange.Run()
+
+	wg.Wait()
+
+	require.Equal(aliceResult, bobPayload)
+	require.Equal(bobResult, alicePayload)
+
+	reunionServer.Shutdown(context.TODO())
+}
