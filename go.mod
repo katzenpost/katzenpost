d1b568392909397ec20eaa8a1ccba884dd381cdb
David Stainton <dstainton415@gmail.com>
2020-03-01 17:21:06 +0000

    servers: add katzenpost mix server plugin

--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/katzenpost/chacha20poly1305 v0.0.0-20191204073434-41b24f726a2c
 	github.com/katzenpost/client v0.0.3-0.20191109165001-aa02bb21ca21
 	github.com/katzenpost/core v0.0.8-0.20191117121951-d5cf686c5a78
+	github.com/katzenpost/server v0.0.8-0.20190910174632-99fb3d5cec86
+	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7
 	github.com/stretchr/testify v1.4.0
 	github.com/ugorji/go/codec v1.1.7
 	golang.org/x/crypto v0.0.0-20191122220453-ac88ee75c92c
---
+++ servers/reunion_katzenpost_server/main.go
@@ -0,0 +1,130 @@
+// main.go - Reunion server using the Katzenpost mix server cbor plugin system.
+// Copyright (C) 2020  David Stainton.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/katzenpost/reunion/commands"
+	"github.com/katzenpost/reunion/epochtime/katzenpost"
+	"github.com/katzenpost/reunion/server"
+	"github.com/katzenpost/server/cborplugin"
+	"github.com/ugorji/go/codec"
+	"gopkg.in/op/go-logging.v1"
+)
+
+func parametersHandler(response http.ResponseWriter, req *http.Request) {
+	params := new(cborplugin.Parameters)
+	var serialized []byte
+	enc := codec.NewEncoderBytes(&serialized, new(codec.CborHandle))
+	if err := enc.Encode(params); err != nil {
+		panic(err)
+	}
+	_, err := response.Write(serialized)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func requestHandler(log *logging.Logger, server *server.Server, response http.ResponseWriter, req *http.Request) {
+	cborHandle := new(codec.CborHandle)
+	request := cborplugin.Request{
+		Payload: make([]byte, 0),
+	}
+	err := codec.NewDecoder(req.Body, new(codec.CborHandle)).Decode(&request)
+	if err != nil {
+		log.Errorf("invalid query command: %s", err.Error())
+		return
+	}
+	cmd, err := commands.FromBytes(request.Payload)
+	if err != nil {
+		log.Errorf("invalid query command: %s", err.Error())
+		return
+	}
+	replyCmd, err := server.ProcessQuery(cmd)
+	if err != nil {
+		log.Errorf("reunion HTTP server invalid reply command: %s", err.Error())
+		return
+	}
+	rawReply := replyCmd.ToBytes()
+	reply := cborplugin.Response{
+		Payload: rawReply,
+	}
+	var serialized []byte
+	enc := codec.NewEncoderBytes(&serialized, cborHandle)
+	if err := enc.Encode(reply); err != nil {
+		log.Error(err.Error())
+		return
+	}
+	log.Debugf("serialized response is len %d", len(serialized))
+	_, err = response.Write(serialized)
+	if err != nil {
+		log.Error(err.Error())
+		return
+	}
+	log.Debug("sent response")
+}
+
+func main() {
+	var logLevel string
+	logPath := flag.String("log", "", "Log file path. Default STDOUT.")
+	flag.StringVar(&logLevel, "log_level", "DEBUG", "logging level could be set to: DEBUG, INFO, NOTICE, WARNING, ERROR, CRITICAL")
+	stateFilePath := flag.String("s", "statefile", "State file path.")
+	epochClockName := flag.String("epochClock", "katzenpost", "The epoch-clock to use.")
+	flag.Parse()
+
+	if *epochClockName != "katzenpost" {
+		panic("Thus far only the Katzenpost epoch clock is supported in this server implementation.")
+	}
+
+	// start service
+	httpServer := http.Server{}
+
+	tmpDir, err := ioutil.TempDir("", "reunion_server")
+	if err != nil {
+		panic(err)
+	}
+	socketFile := filepath.Join(tmpDir, fmt.Sprintf("%d.reunion.socket", os.Getpid()))
+
+	unixListener, err := net.Listen("unix", socketFile)
+	if err != nil {
+		panic(err)
+	}
+
+	// XXX make a reunionServer.
+	clock := new(katzenpost.Clock)
+	reunionServer, err := server.NewServer(clock, *stateFilePath, *logPath, logLevel)
+	if err != nil {
+		panic(err)
+	}
+	httpLog := reunionServer.GetNewLogger("reunion_http_server")
+
+	_requestHandler := func(response http.ResponseWriter, request *http.Request) {
+		requestHandler(httpLog, reunionServer, response, request)
+	}
+	http.HandleFunc("/request", _requestHandler)
+	http.HandleFunc("/parameters", parametersHandler)
+	fmt.Printf("%s\n", socketFile)
+	httpServer.Serve(unixListener)
+	os.Remove(socketFile)
+}
