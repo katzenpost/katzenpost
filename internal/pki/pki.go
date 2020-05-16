0c67ea3988373b753a69004b7c09045afc6c742f
Masala <masala@riseup.net>
2020-05-16 14:55:28 -0400

    fix nextFetchTill

--- internal/pki/pki.go
+++ internal/pki/pki.go
@@ -49,7 +49,7 @@ var (
 	errNotCached         = errors.New("pki: requested epoch document not in cache")
 	recheckInterval      = 1 * time.Minute
 	WarpedEpoch          = "false"
-	nextFetchTill        = epochtime.Period / 2
+	nextFetchTill        = epochtime.Period/8
 	pkiEarlyConnectSlack = epochtime.Period / 6
 )
 
