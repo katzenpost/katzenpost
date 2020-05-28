e46f955325d5c18c9abf7669fa1aeaa034a42981
David Stainton <dstainton415@gmail.com>
2020-05-28 01:20:25 +0000

    servers/katzenpost: add integration test for 2 2-way exchanges

--- servers/reunion_katzenpost_server/client_server_test.go
+++ servers/reunion_katzenpost_server/client_server_test.go
@@ -109,7 +109,7 @@ func register(cfg *config.Config) (*config.Config, *ecdh.PrivateKey) {
 	return cfg, linkKey
 }
 
-func TestDockerClientExchange(t *testing.T) {
+func TestDockerClientExchange1(t *testing.T) {
 	require := require.New(t)
 
 	cfg, err := config.LoadFile("testdata/catshadow.toml")
@@ -196,4 +196,146 @@ func TestDockerClientExchange(t *testing.T) {
 
 	require.Equal(aliceResult, bobPayload)
 	require.Equal(bobResult, alicePayload)
+}
+
+func TestDockerClientExchange2(t *testing.T) {
+	require := require.New(t)
+
+	cfg, err := config.LoadFile("testdata/catshadow.toml")
+	require.NoError(err)
+	cfg, linkKey := register(cfg)
+	c, err := client.New(cfg)
+	require.NoError(err)
+	session, err := c.NewSession(linkKey)
+	require.NoError(err)
+	serviceDesc, err := session.GetService("reunion")
+	require.NoError(err)
+
+	transport := &katzenpost.Transport{
+		Session:   session,
+		Recipient: serviceDesc.Name,
+		Provider:  serviceDesc.Provider,
+	}
+	clock := new(catClock.Clock)
+	epoch, _, _ := clock.Now()
+	srv := []byte{1, 2, 3}
+	passphrase1 := []byte("blah blah motorcycle pencil sharpening gas tank")
+	passphrase2 := []byte("banana republic death squads with itchy trigger fingers")
+
+	f := ""
+	level := "DEBUG"
+	disable := false
+	logBackend, err := log.New(f, level, disable)
+	require.NoError(err)
+
+	var bobResult []byte
+	var aliceResult []byte
+	var nsaResult []byte
+	var gchqResult []byte
+
+	// client Alice
+
+	alicePayload := []byte("Hello Bobby, what's up dude?")
+	aliceContactID := uint64(1)
+	aliceExchangelog := logBackend.GetLogger("alice_exchange")
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	aliceUpdateCh := make(chan reunionClient.ReunionUpdate)
+	go func() {
+		for {
+			update := <-aliceUpdateCh
+			if len(update.Result) > 0 {
+				aliceResult = update.Result
+				fmt.Printf("\n Alice got result: %s\n\n", update.Result)
+				wg.Done()
+			}
+		}
+	}()
+
+	aliceExchange, err := reunionClient.NewExchange(alicePayload, aliceExchangelog, transport, aliceContactID, passphrase1, srv, epoch, aliceUpdateCh)
+	require.NoError(err)
+
+	// bob client
+	bobPayload := []byte("yo Alice, so you are a cryptographer and a language designer both?")
+	bobContactID := uint64(2)
+	bobLogBackend, err := log.New(f, level, disable)
+	require.NoError(err)
+	bobExchangelog := bobLogBackend.GetLogger("bob_exchange")
+
+	wg.Add(1)
+	bobUpdateCh := make(chan reunionClient.ReunionUpdate)
+	go func() {
+		for {
+			update := <-bobUpdateCh
+			if len(update.Result) > 0 {
+				bobResult = update.Result
+				fmt.Printf("\n Bob got result: %s\n\n", update.Result)
+				wg.Done()
+			}
+		}
+	}()
+
+	bobExchange, err := reunionClient.NewExchange(bobPayload, bobExchangelog, transport, bobContactID, passphrase1, srv, epoch, bobUpdateCh)
+	require.NoError(err)
+
+	// NSA client
+	nsaPayload := []byte("yo GCHQ, sup?")
+	nsaContactID := uint64(3)
+	nsaLogBackend, err := log.New(f, level, disable)
+	require.NoError(err)
+	nsaExchangelog := nsaLogBackend.GetLogger("nsa_exchange")
+
+	wg.Add(1)
+	nsaUpdateCh := make(chan reunionClient.ReunionUpdate)
+	go func() {
+		for {
+			update := <-nsaUpdateCh
+			if len(update.Result) > 0 {
+				nsaResult = update.Result
+				fmt.Printf("\n Nsa got result: %s\n\n", update.Result)
+				wg.Done()
+			}
+		}
+	}()
+
+	nsaExchange, err := reunionClient.NewExchange(nsaPayload, nsaExchangelog, transport, nsaContactID, passphrase2, srv, epoch, nsaUpdateCh)
+	require.NoError(err)
+
+	// GCHQ client
+	gchqPayload := []byte("yo NSA, what up")
+	gchqContactID := uint64(4)
+	gchqLogBackend, err := log.New(f, level, disable)
+	require.NoError(err)
+	gchqExchangelog := gchqLogBackend.GetLogger("gchq_exchange")
+
+	wg.Add(1)
+	gchqUpdateCh := make(chan reunionClient.ReunionUpdate)
+	go func() {
+		for {
+			update := <-gchqUpdateCh
+			if len(update.Result) > 0 {
+				gchqResult = update.Result
+				fmt.Printf("\n Gchq got result: %s\n\n", update.Result)
+				wg.Done()
+			}
+		}
+	}()
+
+	gchqExchange, err := reunionClient.NewExchange(gchqPayload, gchqExchangelog, transport, gchqContactID, passphrase2, srv, epoch, gchqUpdateCh)
+	require.NoError(err)
+
+	// Run reunion client exchanges.
+
+	go aliceExchange.Run()
+	go bobExchange.Run()
+	go nsaExchange.Run()
+	go gchqExchange.Run()
+
+	wg.Wait()
+
+	require.Equal(aliceResult, bobPayload)
+	require.Equal(bobResult, alicePayload)
+	require.Equal(nsaResult, gchqPayload)
+	require.Equal(gchqResult, nsaPayload)
 }
