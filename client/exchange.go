329cbf0c9581cf778e52083f821dee44dfba7ed1
Masala <masala@riseup.net>
2020-10-12 16:26:14 -0400

    retransmit on timeout

--- client/exchange.go
+++ client/exchange.go
@@ -24,6 +24,7 @@ import (
 	"fmt"
 	"math/rand"
 
+	"github.com/katzenpost/client"
 	"github.com/katzenpost/reunion/commands"
 	"github.com/katzenpost/reunion/crypto"
 	"github.com/katzenpost/reunion/server"
@@ -585,6 +586,10 @@ func (e *Exchange) Run() {
 		for {
 			// 3:A <- DB: fetch epoch state
 			err := e.fetchState()
+			// if failure due to timeout, retransmit
+			if err == client.ErrReplyTimeout {
+				continue
+			}
 			if err != nil {
 				e.log.Error(err.Error())
 				defer haltedfn()
