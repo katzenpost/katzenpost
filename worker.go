5dfc41c6745ab19dfdcf413bdf4a674107369c7c
Masala <masala@riseup.net>
2021-05-19 14:53:34 -0400

    Revert "send loops not drops for receiver unobservability"
    This reverts commit ab3a68e303a05821ecc1482d95793fd05b494f8b.

--- worker.go
+++ worker.go
@@ -181,6 +181,7 @@ func (s *Session) worker() {
 				} else if lambdaLFired && !s.cfg.Debug.DisableDecoyTraffic {
 					s.sendLoopDecoy(loopSvc)
 				} else if lambdaDFired && !s.cfg.Debug.DisableDecoyTraffic {
+					loopSvc = &loopServices[mrand.Intn(len(loopServices))]
 					s.sendDropDecoy(loopSvc)
 				}
 			}
@@ -236,7 +237,7 @@ func (s *Session) sendFromQueueOrDecoy(loopSvc *utils.ServiceDescriptor) {
 	if err == nil {
 		s.sendNext()
 	} else if !s.cfg.Debug.DisableDecoyTraffic {
-		s.sendLoopDecoy(loopSvc)
+		s.sendDropDecoy(loopSvc)
 	}
 }
 
