0d29b7c7118e36a9b70cb7a166e23b64aa27c643
Masala <masala@riseup.net>
2021-05-23 06:55:38 -0400

    redraw screen once per minute to refresh timestamps

--- catchat.go
+++ catchat.go
@@ -148,6 +148,9 @@ func (a *App) run() error {
 			if err := a.handleGioEvents(e); err != nil {
 				return err
 			}
+		case <-time.After(1*time.Minute):
+			// redraw the screen to update the message timestamps once per minute
+			a.w.Invalidate()
 		}
 	}
 }
