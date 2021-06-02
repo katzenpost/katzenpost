02939696d1f90470e103ed20f431d3b3e58de150
Masala <masala@riseup.net>
2021-06-02 17:25:46 -0400

    show a random avatar when creating contact

--- contact.go
+++ contact.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"gioui.org/layout"
+	"gioui.org/op/paint"
 	"gioui.org/widget"
+	"github.com/benc-uk/gofract/pkg/colors"
+	"github.com/benc-uk/gofract/pkg/fractals"
+	"image"
+	"image/png"
+	"math/rand"
+	//"gioui.org/io/clipboard"
+	"gioui.org/io/pointer"
+	"gioui.org/unit"
 	"gioui.org/widget/material"
 	"github.com/katzenpost/catshadow"
 	"runtime"
@@ -16,10 +26,15 @@ type AddContactComplete struct {
 
 // AddContactPage is the page for adding a new contact
 type AddContactPage struct {
-	nickname *widget.Editor
-	back     *widget.Clickable
-	secret   *widget.Editor
-	submit   *widget.Clickable
+	nickname  *widget.Editor
+	avatar    *widget.Image
+	palette   colors.GradientTable
+	back      *widget.Clickable
+	newAvatar *Click
+	secret    *widget.Editor
+	submit    *widget.Clickable
+	x, y      float64
+	xx, yy    float64
 }
 
 // Layout returns a simple centered layout prompting user for contact nickname and secret
@@ -39,7 +54,13 @@ func (p *AddContactPage) Layout(gtx layout.Context) layout.Dimensions {
 					layout.Flexed(1, fill{th.Bg}.Layout))
 			}),
 			layout.Flexed(1, func(gtx C) D {
-				return layout.Center.Layout(gtx, material.Editor(th, p.nickname, "Nickname").Layout)
+				return layout.Flex{Axis: layout.Horizontal}.Layout(gtx,
+					layout.Flexed(1, func(gtx C) D {
+						return layout.Center.Layout(gtx, material.Editor(th, p.nickname, "Nickname").Layout)
+					}),
+					layout.Flexed(1, func(gtx C) D {
+						return layout.Center.Layout(gtx, p.layoutAvatar)
+					}))
 			}),
 			layout.Flexed(1, func(gtx C) D {
 				return layout.Center.Layout(gtx, material.Editor(th, p.secret, "Secret").Layout)
@@ -62,6 +83,16 @@ func (p *AddContactPage) Event(gtx layout.Context) interface{} {
 			p.secret.Focus()
 		}
 	}
+	for _, e := range p.newAvatar.Events(gtx.Queue) {
+		if e.Type == TypeClick {
+			p.avatar = nil
+			p.xx = rand.Float64()
+			p.x = rand.Float64()
+			p.y = rand.Float64()
+			p.yy = rand.Float64()
+		}
+	}
+
 	for _, ev := range p.secret.Events() {
 		switch ev.(type) {
 		case widget.SubmitEvent:
@@ -75,6 +106,15 @@ func (p *AddContactPage) Event(gtx layout.Context) interface{} {
 			return nil
 		}
 		catshadowClient.NewContact(p.nickname.Text(), []byte(p.secret.Text()))
+		b := &bytes.Buffer{}
+		sz := image.Point{X: gtx.Px(unit.Dp(96)), Y: gtx.Px(unit.Dp(96))}
+		i := image.NewRGBA(image.Rectangle{Max: sz})
+		f := p.fractal(sz)
+		f.Render(i, p.palette)
+
+		if err := png.Encode(b, i); err == nil {
+			catshadowClient.AddBlob("avatar://"+p.nickname.Text(), b.Bytes())
+		}
 		return AddContactComplete{nickname: p.nickname.Text()}
 	}
 	return nil
@@ -88,12 +128,62 @@ func newAddContactPage() *AddContactPage {
 	p.nickname = &widget.Editor{SingleLine: true, Submit: true}
 	p.nickname.Focus()
 	p.back = &widget.Clickable{}
+
+	p.xx = rand.Float64()
+	p.yy = rand.Float64()
+	p.palette = colors.GradientTable{}
+	p.palette.Randomise()
+	p.newAvatar = new(Click)
 	p.secret = &widget.Editor{SingleLine: true, Submit: true}
 	if runtime.GOOS == "android" {
 		p.secret.Submit = false
 	}
 	p.submit = &widget.Clickable{}
 	return p
+}
+
+func (p *AddContactPage) fractal(sz image.Point) *fractals.Fractal {
+	return &fractals.Fractal{FractType: "julia",
+		Center:       fractals.ComplexPair{p.x, p.y},
+		MagFactor:    1.0,
+		MaxIter:      90,
+		W:            3.0,
+		H:            2.0,
+		ImgWidth:     sz.X,
+		JuliaSeed:    fractals.ComplexPair{p.xx, p.yy},
+		InnerColor:   "#000000",
+		FullScreen:   false,
+		ColorRepeats: 2.0}
+}
+
+func (p *AddContactPage) layoutAvatar(gtx C) D {
+	scale := 1.0
+	in := layout.Inset{Left: unit.Dp(0), Right: unit.Dp(0)}
+	cc := clipCircle{}
+
+	return in.Layout(gtx, func(gtx C) D {
+		dims := cc.Layout(gtx, func(gtx C) D {
+			y := gtx.Constraints.Max.Y / 2
+			sz := image.Point{X: y, Y: y}
+
+			gtx.Constraints = layout.Exact(gtx.Constraints.Constrain(sz))
+			if p.avatar != nil {
+				return p.avatar.Layout(gtx)
+			}
+			f := p.fractal(sz)
+
+			i := image.NewRGBA(image.Rectangle{Max: sz})
+			p.palette.Randomise()
+			f.Render(i, p.palette)
+			p.avatar = &widget.Image{Scale: float32(scale), Src: paint.NewImageOp(i)}
+			return p.avatar.Layout(gtx)
+
+		})
+		a := pointer.Rect(image.Rectangle{Max: dims.Size})
+		a.Add(gtx.Ops)
+		p.newAvatar.Add(gtx.Ops)
+		return dims
+	})
 }
 
 type sortedContacts []*catshadow.Contact
