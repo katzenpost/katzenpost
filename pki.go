f580d703cc4b5777793c0c967d87b7c786595c74
Masala <masala@riseup.net>
2021-06-21 20:08:02 -0400

    adjust pki fetch schedule to authority

--- pki.go
+++ pki.go
@@ -33,7 +33,7 @@ import (
 var (
 	errGetConsensusCanceled = errors.New("minclient/pki: consensus fetch canceled")
 	errConsensusNotFound    = errors.New("minclient/pki: consensus not ready yet")
-	nextFetchTill           = epochtime.Period / 8
+	nextFetchTill           = 3 * (epochtime.Period / 8)
 	recheckInterval         = 1 * time.Minute
 	// WarpedEpoch is a build time flag that accelerates the recheckInterval
 	WarpedEpoch = "false"
