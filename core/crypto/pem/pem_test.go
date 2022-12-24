cafd77f8d27a97524bbd5ee5e22d52465f7bb297
Masala <masala@riseup.net>
2022-12-24 11:42:45 +0000

    crypto: move pem tests into pem

--- core/crypto/pem/pem_test.go
+++ core/crypto/pem/pem_test.go
@@ -37,6 +37,21 @@ func TestToFromPEM(t *testing.T) {
 
 }
 
+func TestToFromPEM(t *testing.T) {
+	verifyKeyString1 := `-----BEGIN ED25519 SPHINCS+ PUBLIC KEY-----
+3zXGXWKZeW7cgLRKZPeF73Yi0+J+BtEzfoHM1SXMqdB30p0iAy1JDzaLWPOdAJNq
+jWmeAbCVBJkDkRtdzghri6me5ZgEwQ3tr/OVK2Podxl3dIKP+riONi3po+D57ryg
+-----END ED25519 SPHINCS+ PUBLIC KEY-----
+`
+	_, pubKey := Scheme.NewKeypair()
+
+	err := pem.FromPEMString(verifyKeyString1, pubKey)
+	require.NoError(t, err)
+
+	verifyKeyString2 := string(pem.ToPEMBytes(pubKey))
+	require.Equal(t, verifyKeyString1, verifyKeyString2)
+}
+
 func TestExistsAndNotExists(t *testing.T) {
 	tmpdir, err := os.MkdirTemp("", "TestBothExists")
 	require.NoError(t, err)
