e46f9a3284fe010db66e6c599d92986084df7c0d
David Stainton <dstainton415@gmail.com>
2022-12-30 11:38:09 -0500

    Remove superfluous comments from hybrid module

--- core/crypto/nike/hybrid/hybrid.go
+++ core/crypto/nike/hybrid/hybrid.go
@@ -4,38 +4,32 @@ import (
 	"github.com/katzenpost/katzenpost/core/crypto/nike"
 )
 
-// Public key of a hybrid NIKE.
 type publicKey struct {
 	scheme *scheme
 	first  nike.PublicKey
 	second nike.PublicKey
 }
 
-// Private key of a hybrid NIKE.
 type privateKey struct {
 	scheme *scheme
 	first  nike.PrivateKey
 	second nike.PrivateKey
 }
 
-// Scheme for a hybrid NIKE.
 type scheme struct {
 	name   string
 	first  nike.Scheme
 	second nike.Scheme
 }
 
-// PublicKeySize returns the size in bytes of the public key.
 func (s *scheme) PublicKeySize() int {
 	return s.first.PublicKeySize() + s.second.PublicKeySize()
 }
 
-// PrivateKeySize returns the size in bytes of the private key.
 func (s *scheme) PrivateKeySize() int {
 	return s.first.PrivateKeySize() + s.second.PrivateKeySize()
 }
 
-// NewKeypair returns a newly generated key pair.
 func (s *scheme) NewKeypair() (nike.PrivateKey, nike.PublicKey) {
 	privKey1, pubKey1 := s.first.NewKeypair()
 	privKey2, pubKey2 := s.second.NewKeypair()
@@ -50,14 +44,11 @@ func (s *scheme) NewKeypair() (nike.PrivateKey, nike.PublicKey) {
 		}
 }
 
-// DeriveSecret derives a shared secret given a private key
-// from one party and a public key from another.
 func (s *scheme) DeriveSecret(privKey nike.PrivateKey, pubKey nike.PublicKey) []byte {
 	return append(privKey.(*privateKey).scheme.first.DeriveSecret(privKey.(*privateKey).first, pubKey.(*publicKey).first),
 		privKey.(*privateKey).scheme.second.DeriveSecret(privKey.(*privateKey).second, pubKey.(*publicKey).second)...)
 }
 
-// DerivePublicKey derives a public key given a private key.
 func (s *scheme) DerivePublicKey(privKey nike.PrivateKey) nike.PublicKey {
 	return &publicKey{
 		scheme: s,
@@ -66,16 +57,6 @@ func (s *scheme) DerivePublicKey(privKey nike.PrivateKey) nike.PublicKey {
 	}
 }
 
-// Blind performs the blinding operation against the
-// two byte slices and returns the blinded value.
-//
-// Note that the two arguments must be the correct lengths:
-//
-// * groupMember must be the size of a public key.
-//
-// * blindingFactor must be the size of a private key.
-//
-// See also PublicKey's Blind method.
 func (s *scheme) Blind(groupMember []byte, blindingFactor []byte) (blindedGroupMember []byte) {
 	if len(groupMember) != s.PublicKeySize() {
 		panic("invalid group member size")
@@ -95,7 +76,6 @@ func (s *scheme) NewEmptyPublicKey() nike.PublicKey {
 	}
 }
 
-// UnmarshalBinaryPublicKey loads a public key from byte slice.
 func (s *scheme) UnmarshalBinaryPublicKey(b []byte) (nike.PublicKey, error) {
 	pubkey := s.NewEmptyPublicKey()
 	err := pubkey.FromBytes(b)
@@ -122,8 +102,6 @@ func (p *privateKey) FromBytes(b []byte) error {
 	return p.second.FromBytes(b[p.scheme.first.PrivateKeySize():])
 }
 
-// Blind performs a blinding operation and mutates the public
-// key with the blinded value.
 func (p *publicKey) Blind(blindingFactor []byte) error {
 	err := p.first.Blind(blindingFactor[:p.scheme.first.PublicKeySize()])
 	if err != nil {
