cf969dd3c02cef11b518c3eaec889b664b38afa1
David Stainton <dstainton415@gmail.com>
2022-12-30 11:42:03 -0500

    Add CTIDHX25519

--- core/crypto/nike/hybrid/hybrid.go
+++ core/crypto/nike/hybrid/hybrid.go
@@ -2,7 +2,16 @@ package hybrid
 
 import (
 	"github.com/katzenpost/katzenpost/core/crypto/nike"
+	"github.com/katzenpost/katzenpost/core/crypto/nike/ctidh"
+	"github.com/katzenpost/katzenpost/core/crypto/nike/ecdh"
+	"github.com/katzenpost/katzenpost/core/crypto/rand"
 )
+
+var CTIDHX25519 nike.Scheme = &scheme{
+	name:   "CTIDH-X25519",
+	first:  ctidh.CTIDHScheme,
+	second: ecdh.NewEcdhNike(rand.Reader),
+}
 
 type publicKey struct {
 	scheme *scheme
