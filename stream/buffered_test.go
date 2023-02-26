6e06ef2737691586af5d1d10707d1b20d8a0e6e2
Masala <masala@riseup.net>
2023-02-26 14:09:51 +0000

    Stream: Add Buffered CBOR decoder type for deserializing CBOR without losing bytes read form Stream

---
+++ stream/buffered_test.go
@@ -0,0 +1,155 @@
+package stream
+
+import (
+	"encoding/binary"
+	"errors"
+	"io"
+	"net/http"
+	_ "net/http/pprof"
+	"runtime"
+	"sync"
+	"testing"
+
+	"github.com/fxamacker/cbor/v2"
+	"github.com/katzenpost/katzenpost/core/crypto/rand"
+	"github.com/stretchr/testify/require"
+)
+
+var numEntries = 42
+
+type mockTransport struct {
+	l    *sync.Mutex
+	data map[string][]byte
+}
+
+// newStreams returns an initialized pair of Streams
+func newStreams(t Transport) (*Stream, *Stream) {
+	a := newStream(t)
+	addr := &StreamAddr{address: generate()}
+	a.keyAsListener(addr)
+	b := newStream(t)
+	b.keyAsDialer(addr)
+
+	if a == nil || b == nil {
+		panic("wtf")
+	}
+	return a, b
+}
+
+func NewMockTransport() Transport {
+	m := &mockTransport{}
+	m.l = new(sync.Mutex)
+	m.l.Lock()
+	defer m.l.Unlock()
+	m.data = make(map[string][]byte)
+	return m
+}
+
+func (m mockTransport) Put(addr []byte, payload []byte) error {
+	m.l.Lock()
+	defer m.l.Unlock()
+	m.data[string(addr)] = payload
+	return nil
+}
+
+func (m mockTransport) Get(addr []byte) ([]byte, error) {
+	m.l.Lock()
+	defer m.l.Unlock()
+	d, ok := m.data[string(addr)]
+	if !ok {
+		return nil, errors.New("NotFound")
+	}
+	return d, nil
+}
+
+func randPayload() []byte {
+	l := rand.NewMath().Intn(1 << 16)
+	buf := make([]byte, l)
+	io.ReadFull(rand.Reader, buf)
+	for i := 0; i < l; i++ {
+		buf[i] = 0x42
+	}
+	return buf
+}
+
+func TestMockTransport(t *testing.T) {
+	garbage := NewMockTransport()
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
+	go func() {
+		addr := make([]byte, 8)
+		for i := 0; i < 4096; i++ {
+			binary.BigEndian.PutUint64(addr, uint64(i))
+			garbage.Put(addr, randPayload())
+		}
+	}()
+	go func() {
+		addr := make([]byte, 8)
+		for i := 0; i < 4096; i++ {
+			binary.BigEndian.PutUint64(addr, uint64(i))
+			garbage.Put(addr, randPayload())
+		}
+	}()
+
+}
+
+type msg struct {
+	Num     int
+	Name    string
+	Payload []byte
+}
+
+func TestBufferedStream(t *testing.T) {
+	require := require.New(t)
+	trans := NewMockTransport()
+	a, b := newStreams(trans)
+	a.Start()
+	b.Start()
+
+	sent := make([]*msg, 0)
+	recv := make([]*msg, 0)
+	enc := cbor.NewEncoder(a)
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
+	// write data to receiver
+	go func() {
+		for i := 0; i < numEntries; i++ {
+			hello := &msg{Num: i, Name: "Tester", Payload: randPayload()}
+			sent = append(sent, hello)
+			err := enc.Encode(hello)
+			require.NoError(err)
+			t.Logf("sent data %d", i)
+		}
+		//a.Close() // XXX: Stream.WriteBuf isn't drained yet!
+		wg.Done()
+	}()
+	bs := BufferedStream{Stream: b}
+	// read data
+	go func() {
+		for i := 0; i < numEntries; i++ {
+			r := new(msg)
+			err := bs.CBORDecode(r)
+			if err == io.EOF {
+				require.Equal(i, numEntries-1)
+				break
+			}
+			require.NoError(err)
+			recv = append(recv, r)
+			t.Logf("recv data %d", i)
+		}
+		b.Close()
+		wg.Done()
+	}()
+	wg.Wait()
+	for i := 0; i < numEntries; i++ {
+		require.Equal(sent[i], recv[i])
+	}
+}
+
+func init() {
+	go func() {
+		http.ListenAndServe("localhost:8181", nil)
+	}()
+	runtime.SetMutexProfileFraction(1)
+	runtime.SetBlockProfileRate(1)
+}
