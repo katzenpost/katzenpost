63a09e26ad8945c1a2e9893090785fb15941a985
David Stainton <dstainton415@gmail.com>
2023-05-21 09:46:13 -0400

    KEM adapter test nitpick correction

--- core/crypto/kem/adapter/kem_test.go
+++ core/crypto/kem/adapter/kem_test.go
@@ -18,13 +18,13 @@ func TestNikeToKemAdapter(t *testing.T) {
 	pubkey1, privkey1, err := s.GenerateKeyPair()
 	require.NoError(t, err)
 
-	ct, ss1, err := s.Encapsulate(pubkey1)
+	ct, ssA, err := s.Encapsulate(pubkey1)
 	require.NoError(t, err)
 
-	ss2, err := s.Decapsulate(privkey1, ct)
+	ssB, err := s.Decapsulate(privkey1, ct)
 	require.NoError(t, err)
 
-	require.Equal(t, ss1, ss2)
+	require.Equal(t, ssA, ssB)
 
-	t.Logf("our shared key is %x", ss1)
+	t.Logf("our shared key is %x", ssA)
 }
