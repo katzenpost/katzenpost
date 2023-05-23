3ccab8cfcf4cf886d6ac14254f152d64011a8972
David Stainton <dstainton415@gmail.com>
2023-05-23 09:59:39 -0400

    Remove nobs csidh 512 from nike schmes list

--- core/crypto/nike/schemes/schemes.go
+++ core/crypto/nike/schemes/schemes.go
@@ -4,7 +4,6 @@ import (
 	"strings"
 
 	"github.com/katzenpost/katzenpost/core/crypto/nike"
-	"github.com/katzenpost/katzenpost/core/crypto/nike/csidh"
 	"github.com/katzenpost/katzenpost/core/crypto/nike/ecdh"
 	"github.com/katzenpost/katzenpost/core/crypto/nike/hybrid"
 	"github.com/katzenpost/katzenpost/core/crypto/rand"
@@ -13,7 +12,6 @@ import (
 // NOTE(david): The CTIDH schemes won't work unless you build with
 // "ctidh" build tag.
 var allSchemes = [...]nike.Scheme{
-	csidh.NOBS_CSIDH512Scheme,
 	ecdh.NewEcdhNike(rand.Reader),
 	hybrid.NOBS_CSIDH512X25519,
 	// Must build with `ctidh` build tag (and other supporting env vars)
