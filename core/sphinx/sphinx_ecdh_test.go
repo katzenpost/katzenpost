332182f058d3f79eef20c0d032b8d4cdaa2b5857
David Stainton <dstainton415@gmail.com>
2023-05-29 17:24:59 -0400

    Add a simple ecdh sphinx test

--- core/sphinx/sphinx_ecdh_test.go
+++ core/sphinx/sphinx_ecdh_test.go
@@ -107,3 +107,34 @@ func TestSphinxProductionSimili(t *testing.T) {
 
 	require.True(t, len(pkt) == g.PacketLength)
 }
+
+func TestSphinxOneHop(t *testing.T) {
+	nrHops := 1
+	mynike := ecdh.NewEcdhNike(rand.Reader)
+
+	r := require.New(t)
+	_, fwdPath := newNikePathVector(r, mynike, nrHops, true)
+	_, revPath := newNikePathVector(r, mynike, nrHops, true)
+
+	g := geo.GeometryFromUserForwardPayloadLength(mynike, 2000, true, nrHops)
+
+	s, err := FromGeometry(g)
+	require.NoError(t, err)
+
+	zeroBytes := make([]byte, g.UserForwardPayloadLength)
+	payload := make([]byte, 2, g.ForwardPayloadLength)
+	payload[0] = 1 // Packet has a SURB.
+
+	surb, _, err := s.NewSURB(rand.Reader, revPath)
+	require.NoError(t, err)
+
+	payload = append(payload, surb...)
+	payload = append(payload, zeroBytes...)
+
+	require.True(t, len(payload) == g.ForwardPayloadLength)
+
+	pkt, err := s.NewPacket(rand.Reader, fwdPath, payload)
+	require.NoError(t, err)
+
+	require.True(t, len(pkt) == g.PacketLength)
+}
