335c09ff7271eee74bd4dd85321c52e2e93735a2
Masala <masala@riseup.net>
2023-06-03 19:26:17 +0000

    sockatz/client: start adding end-to-end tests

---
+++ sockatz/client/client_test.go
@@ -0,0 +1,81 @@
+// client_test.go - stream socket client tests
+// Copyright (C) 2023  Masala
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//go:build docker_test
+// +build docker_test
+
+package client
+
+import (
+	"github.com/katzenpost/katzenpost/core/crypto/rand"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestDockerNewClient(t *testing.T) {
+	require := require.New(t)
+
+	cfg, err := config.LoadFile("testdata/client.toml")
+	require.NoError(err)
+	session, err := GetSession(cfg)
+	require.NoError(err)
+	require.NotNil(session)
+	c, err := NewClient(session)
+	require.NoError(err)
+	require.NotNil(c)
+}
+
+func TestDockerProxy(t *testing.T) {
+	require := require.New(t)
+
+	cfg, err := config.LoadFile("testdata/client.toml")
+	require.NoError(err)
+	session, err := GetSession(cfg)
+	require.NoError(err)
+	require.NotNil(session)
+	c, err := NewClient(session)
+	require.NoError(err)
+	require.NotNil(c)
+
+	// make a unique id
+	id := make([]byte, 32)
+	_, err := io.ReadFull(rand.Reader, id)
+	require.NoError(err)
+
+	// add some credit
+	err := <-c.Topup()
+	require.NoError(err)
+
+	// open a listening socket
+	r := net.ListenTCP("tcp", &net.TCPAddr{})
+
+	// dial our socket with proxy
+	err := <-c.Dial(id, u)
+	require.NoError(err)
+
+	l := net.ListenTCP("tcp", &net.TCPAddr{})
+	proxyCh := c.Proxy(id, l)
+
+	payload := make([]byte, 42)
+	proxiedpayload := make([]byte, 42)
+
+	_, err := l.Write(payload)
+	require.NoError(err)
+
+	_, err = r.Read(proxiedpayload)
+	require.NoError(err)
+	require.True(bytes.Equal(proxiedpayload, payload))
+}
