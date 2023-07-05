2166df0a1097d2aa167c6d8c137b0900910d0e9b
Masala <masala@riseup.net>
2023-07-05 15:39:59 +0000

    sockatz: re-add client main.go
    This re-adds the SOCKS5 client application

---
+++ sockatz/client/cmd/client/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"github.com/katzenpost/katzenpost/sockatz/client"
+
+	"flag"
+	"fmt"
+	"net"
+	"sync"
+)
+
+var (
+	cfgFile = flag.String("cfg", "sockatz.toml", "config file")
+	port    = flag.Int("port", 4242, "listener address")
+)
+
+// getSession waits until pki.Document is available and returns a *client.Session
+func main() {
+	flag.Parse()
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		panic(err)
+	}
+
+	s, err := client.GetSession(*cfgFile)
+	if err != nil {
+		panic(err)
+	}
+
+	c, err := client.NewClient(s)
+	if err != nil {
+		panic(err)
+	}
+	if err != nil {
+		panic(err)
+	}
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	go func() {
+		_ = socksAcceptLoop(c, ln)
+		wg.Done()
+	}()
+	/*
+		TODO: Add a HTTP3 CONNECT proxy listener that uses QUIC Datagram to proxy QUIC UDP connections
+		wg.Add(1)
+		go func() {
+			_ = httpAcceptLoop(s, ..
+	*/
+	// wait until loop has exited
+	wg.Wait()
+}
+
+func socksAcceptLoop(c *client.Client, ln net.Listener) error {
+	defer ln.Close()
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			if e, ok := err.(net.Error); ok && !e.Temporary() {
+				return err
+			}
+			continue
+		}
+		go c.SocksHandler(conn)
+	}
+}
