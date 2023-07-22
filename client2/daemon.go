0c837f4cca81a7dc59e225734898e410d694be01
David Stainton <dstainton415@gmail.com>
2023-07-21 18:54:47 -0700

    client2 daemon fanout wip

--- client2/daemon.go
+++ client2/daemon.go
@@ -1,41 +1,40 @@
 package client2
 
-import "github.com/katzenpost/katzenpost/core/worker"
+import (
+	"github.com/katzenpost/katzenpost/client2/config"
+	"github.com/katzenpost/katzenpost/core/worker"
+)
 
 type Daemon struct {
 	worker.Worker
 
-	client      *Client
-	listener    *listener
-	decoySender *decoySender
-	egressCh    chan *Request
+	cfg      *config.Config
+	client   *Client
+	listener *listener
+	egressCh chan *Request
 }
 
-func NewDaemon() (*Daemon, error) {
+func NewDaemon(cfg *config.Config, egressSize int) (*Daemon, error) {
 	return &Daemon{
-		egressCh: make(chan *Request),
+		cfg:      cfg,
+		egressCh: make(chan *Request, egressSize),
 	}, nil
 }
 
-func (d *Daemon) egressWorker() {
-	for {
-		select {
-		case <-d.HaltCh():
-			return
-		case <-d.egressCh:
-			//d.client.SendSphinxPacket()
-		}
-	}
-}
-
 func (d *Daemon) Start() error {
+	rates := &Rates{}
+	if d.cfg.CachedDocument != nil {
+		rates = ratesFromPKIDoc(d.cfg.CachedDocument)
+	}
+	var err error
+	d.listener, err = NewListener(rates, d.egressCh)
+	if err != nil {
+		return err
+	}
+	d.client, err = New(d.cfg)
+	if err != nil {
+		return err
+	}
 
-	/*
-		rates := &Rates{}
-		listener, err := NewListener(rates, egressCh)
-		if err != nil {
-			return err
-		}
-	*/
 	return nil
 }
