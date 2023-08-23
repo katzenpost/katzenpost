e0dde7105d382bb638c5a5df1bb16a71b0209afb
David Stainton <dstainton415@gmail.com>
2023-08-23 09:37:14 -0700

    client2 launcher might be useful

---
+++ client2/launcher.go
@@ -0,0 +1,33 @@
+package client2
+
+import (
+	"os"
+	"syscall"
+)
+
+type ClientLauncher struct {
+	process *os.Process
+}
+
+func (l *ClientLauncher) Halt() {
+	err := l.process.Signal(syscall.SIGHUP)
+	if err != nil {
+		panic(err)
+	}
+	_, err = l.process.Wait()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (l *ClientLauncher) Launch(args ...string) error {
+	var procAttr os.ProcAttr
+	procAttr.Files = []*os.File{os.Stdin,
+		os.Stdout, os.Stderr}
+	var err error
+	l.process, err = os.StartProcess(args[0], args, &procAttr)
+	if err != nil {
+		return err
+	}
+	return nil
+}
