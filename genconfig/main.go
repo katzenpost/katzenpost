4f3ea6ba659c7aa96e45ffa9bf3465fd30c058a7
Masala <masala@riseup.net>
2023-09-09 10:31:51 +0000

    cloaked mvp: disable unused services

--- genconfig/main.go
+++ genconfig/main.go
@@ -187,16 +187,6 @@ func (s *katzenpost) genNodeConfig(isProvider bool, isVoting bool, transports []
 			cfg.Provider.TrustOnFirstUse = true
 			cfg.Provider.EnableEphemeralClients = true
 		} else {
-			spoolCfg := &sConfig.CBORPluginKaetzchen{
-				Capability:     "spool",
-				Endpoint:       "+spool",
-				Command:        s.baseDir + "/memspool" + s.binSuffix,
-				MaxConcurrency: 1,
-				Config: map[string]interface{}{
-					"data_store": s.baseDir + "/" + cfg.Server.Identifier + "/memspool.storage",
-					"log_dir":    s.baseDir + "/" + cfg.Server.Identifier,
-				},
-			}
 			katzensocksCfg := &sConfig.CBORPluginKaetzchen{
 				Capability:     "katzensocks",
 				Endpoint:       "+katzensocks",
@@ -210,23 +200,7 @@ func (s *katzenpost) genNodeConfig(isProvider bool, isVoting bool, transports []
 				},
 			}
 
-			cfg.Provider.CBORPluginKaetzchen = []*sConfig.CBORPluginKaetzchen{spoolCfg, katzensocksCfg}
-			if !s.hasPanda {
-				pandaCfg := &sConfig.CBORPluginKaetzchen{
-					Capability:     "panda",
-					Endpoint:       "+panda",
-					Command:        s.baseDir + "/panda_server" + s.binSuffix,
-					MaxConcurrency: 1,
-					Config: map[string]interface{}{
-						"fileStore": s.baseDir + "/" + cfg.Server.Identifier + "/panda.storage",
-						"log_dir":   s.baseDir + "/" + cfg.Server.Identifier,
-						"log_level": "DEBUG",
-					},
-				}
-				cfg.Provider.CBORPluginKaetzchen = append(cfg.Provider.CBORPluginKaetzchen, pandaCfg)
-				s.hasPanda = true
-			}
-
+			cfg.Provider.CBORPluginKaetzchen = []*sConfig.CBORPluginKaetzchen{katzensocksCfg}
 			// Add an instance of a http proxy for the Cashu mint service listening on 3338
 			if !s.hasProxy {
 				proxyCfg := &sConfig.CBORPluginKaetzchen{
@@ -235,7 +209,6 @@ func (s *katzenpost) genNodeConfig(isProvider bool, isVoting bool, transports []
 					Command:        s.baseDir + "/proxy_server" + s.binSuffix,
 					MaxConcurrency: 1,
 					Config: map[string]interface{}{
-						// allow connections to localhost:4242
 						"host":      "localhost:3338",
 						"log_dir":   s.baseDir + "/" + cfg.Server.Identifier,
 						"log_level": "DEBUG",
