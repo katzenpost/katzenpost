a9f345a70412353463539156201d25bd8329c258
Jacob Appelbaum <jacob@appelbaum.net>
2023-12-06 20:10:52 +0100

    remove extern void randombytes and related define

--- sphincsplus/ref/randombytes.h
+++ sphincsplus/ref/randombytes.h
@@ -1,8 +1,6 @@
 #ifndef SPX_RANDOMBYTES_H
 #define SPX_RANDOMBYTES_H
 
-extern void randombytes(unsigned char * x,unsigned long long xlen);
-
-#define _randombytes(x, xlen) randombytes(x, xlen)
+void _randombytes(unsigned char * x,unsigned long long xlen);
 
 #endif
