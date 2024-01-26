2e299a196d4397d5a507bdc65dc1db67a413602f
David Stainton <dstainton415@gmail.com>
2024-01-26 13:53:22 -0800

    client2: arq, add missing logging line

--- client2/arq.go
+++ client2/arq.go
@@ -174,6 +174,8 @@ func (a *ARQ) Has(surbID *[sConstants.SURBIDLength]byte) bool {
 // the APP ID and SURB Key so that the reply and be decrypted and routed
 // to the correct application.
 func (a *ARQ) HandleAck(surbID *[sConstants.SURBIDLength]byte) (*replyDescriptor, error) {
+	a.log.Info("HandleAck")
+
 	a.lock.Lock()
 	defer a.lock.Unlock()
 
