add1993189521c2df39b2681048d602e129554e3
David Stainton <dstainton415@gmail.com>
2024-02-03 11:57:33 -0800

    client2: add some pki and listener unit tests

--- client2/pki_test.go
+++ client2/pki_test.go
@@ -1,8 +1,13 @@
+// SPDX-FileCopyrightText: Copyright (C) 2024 David Stainton
+// SPDX-License-Identifier: AGPL-3.0-only
+
 package client2
 
 import (
 	"context"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/katzenpost/katzenpost/client2/config"
 	"github.com/katzenpost/katzenpost/core/crypto/rand"
@@ -53,7 +58,7 @@ func TestClientPKIStartStop(t *testing.T) {
 	p.Wait()
 }
 
-func TestGetDocument(t *testing.T) {
+func TestPKIGetDocument(t *testing.T) {
 	cfg, err := config.LoadFile("testdata/client.toml")
 	require.NoError(t, err)
 
@@ -86,7 +91,7 @@ func TestGetDocument(t *testing.T) {
 	require.Nil(t, doc)
 }
 
-func TestUpdateDocumentBadSphinxHash(t *testing.T) {
+func TestPKIUpdateDocumentBadSphinxHash(t *testing.T) {
 	cfg, err := config.LoadFile("testdata/client.toml")
 	require.NoError(t, err)
 
@@ -122,7 +127,7 @@ func TestUpdateDocumentBadSphinxHash(t *testing.T) {
 	require.Error(t, err)
 }
 
-func TestUpdateDocument(t *testing.T) {
+func TestPKIUpdateDocument(t *testing.T) {
 	cfg, err := config.LoadFile("testdata/client.toml")
 	require.NoError(t, err)
 
@@ -157,4 +162,127 @@ func TestUpdateDocument(t *testing.T) {
 	doc := p.currentDocument()
 	require.NotNil(t, doc)
 	require.Equal(t, testDoc, doc)
+}
+
+func TestPKIWaitForDocument(t *testing.T) {
+	cfg, err := config.LoadFile("testdata/client.toml")
+	require.NoError(t, err)
+
+	myMockPKIClient := new(mockPKIClient)
+	c := &Client{
+		cfg:       cfg,
+		PKIClient: myMockPKIClient,
+	}
+
+	p := newPKI(c)
+	c.pki = p
+	p.consensusGetter = new(mockConsensusGetter)
+
+	epoch, _, _ := epochtime.Now()
+
+	testDoc := &cpki.Document{
+		Epoch:              epoch,
+		SphinxGeometryHash: c.cfg.SphinxGeometry.Hash(),
+	}
+
+	myMockPKIClient.doc = testDoc
+
+	require.Nil(t, p.currentDocument())
+	c.WaitForCurrentDocument()
+	require.NotNil(t, p.currentDocument())
+	require.Equal(t, p.currentDocument(), testDoc)
+}
+
+func TestPKIClockSkew(t *testing.T) {
+	cfg, err := config.LoadFile("testdata/client.toml")
+	require.NoError(t, err)
+
+	myMockPKIClient := new(mockPKIClient)
+	c := &Client{
+		cfg:       cfg,
+		PKIClient: myMockPKIClient,
+	}
+	p := newPKI(c)
+	c.pki = p
+	p.consensusGetter = new(mockConsensusGetter)
+
+	epoch, _, _ := epochtime.Now()
+	ctx := context.TODO()
+
+	myMockPKIClient.doc = &cpki.Document{
+		Epoch: epoch,
+	}
+
+	doc, err := p.getDocument(ctx, epoch)
+	require.NoError(t, err)
+	require.NotNil(t, doc)
+	require.Equal(t, doc.Epoch, epoch)
+
+	skew := int64(1234)
+	p.setClockSkew(skew)
+	skewDuration := c.ClockSkew()
+	expected := time.Duration(skew) * time.Second
+	require.Equal(t, expected, skewDuration)
+}
+
+func lenSyncMap(m *sync.Map) int {
+	var i int
+	m.Range(func(k, v interface{}) bool {
+		i++
+		return true
+	})
+	return i
+}
+
+func TestPKICachedDoc(t *testing.T) {
+	cfg, err := config.LoadFile("testdata/client.toml")
+	require.NoError(t, err)
+
+	myMockPKIClient := new(mockPKIClient)
+	c := &Client{
+		cfg:       cfg,
+		PKIClient: myMockPKIClient,
+	}
+	p := newPKI(c)
+	require.Equal(t, 0, lenSyncMap(&p.docs))
+
+	epoch, _, _ := epochtime.Now()
+
+	doc1 := &cpki.Document{
+		Epoch: epoch,
+	}
+	doc2 := &cpki.Document{
+		Epoch: epoch + 1,
+	}
+	doc3 := &cpki.Document{
+		Epoch: epoch + 2,
+	}
+	c.cfg.CachedDocument = doc1
+
+	p = newPKI(c)
+	c.pki = p
+	require.Equal(t, 1, lenSyncMap(&p.docs))
+
+	p.pruneDocuments(epoch)
+	require.Equal(t, 1, lenSyncMap(&p.docs))
+
+	p.pruneDocuments(epoch + 1)
+	require.Equal(t, 0, lenSyncMap(&p.docs))
+
+	p = newPKI(c)
+	c.pki = p
+	require.Equal(t, 1, lenSyncMap(&p.docs))
+
+	myMockPKIClient.doc = doc2
+	ctx := context.TODO()
+	p.consensusGetter = new(mockConsensusGetter)
+	doc, err := p.getDocument(ctx, doc2.Epoch)
+	require.NoError(t, err)
+	require.Equal(t, doc2, doc)
+
+	myMockPKIClient.doc = doc3
+	ctx = context.TODO()
+	doc, err = p.getDocument(ctx, doc3.Epoch)
+	require.NoError(t, err)
+	require.Equal(t, doc3, doc)
 }
