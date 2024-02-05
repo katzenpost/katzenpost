d7e89c14012c08831e581fff0b79770091dc053c
David Stainton <dstainton415@gmail.com>
2024-02-05 10:56:40 -0800

    server: fix internal pki.go

--- server/internal/pki/pki.go
+++ server/internal/pki/pki.go
@@ -679,7 +679,6 @@ func New(glue glue.Glue) (glue.PKI, error) {
 	}
 
 	pkiCfg := &vClient.Config{
-		DataDir:     glue.Config().Server.DataDir,
 		LinkKey:     glue.LinkKey(),
 		LogBackend:  glue.LogBackend(),
 		Authorities: glue.Config().PKI.Voting.Authorities,
