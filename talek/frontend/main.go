6361c5d371e6846c22556b095f74e345197d462f
Masala <masala@riseup.net>
2024-02-16 15:46:13 +0000

    talek/frontend: add configuration only mode, create TrustDomain for Frontend
    adds flag -g to toggle generating client configuration and exiting
    
    TODO: save the TrustDomain generated for the frontend so that the frontend can be restarted

--- talek/frontend/main.go
+++ talek/frontend/main.go
@@ -127,14 +127,16 @@ func NewKPFrontendServer(name string, session *client.Session, serverConfig *ser
 func main() {
 	var clientConfigPath string
 	var kpConfigPath string
-	var listen string
+	var listenAddress string
 	var verbose bool
+	var configOnly bool
 
 	// add mixnet config
 	flag.StringVar(&clientConfigPath, "config", "client.json", "File to write Talek Client Configuration")
 	flag.StringVar(&kpConfigPath, "kpconfig", "client.toml", "Katzenpost Configuration")
-	flag.StringVar(&listen, "listen", ":8080", "Listening Address")
+	flag.StringVar(&listenAddress, "listen", "localhost:8080", "Listening Address")
 	flag.BoolVar(&verbose, "verbose", false, "Verbose output")
+	flag.BoolVar(&configOnly, "g", false, "write client configuration and exit")
 	flag.Parse()
 
 	// load katzenpost client configuration
@@ -215,14 +217,59 @@ func main() {
 		// make a replica instance
 		replicas = append(replicas, NewReplicaKPC(desc.Name, desc.Provider, session, trustDomainCfg))
 	}
-	// XXX // check that all replicas agree on parameters
+	// TODO check that all replicas agree on parameters
 
-	// create a server.Config
+	// marshal common.Config into indented json and print parameters
+	commonCfg := commonCfgs[0]
+	cfgJson, err := json.MarshalIndent(commonCfg, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+	fmt.Printf("received common.Config:\n'%v'\n", string(cfgJson))
+
+	// create a TrustDomainConfig for this Frontend
+	frontendServerTrustDomain := tCommon.NewTrustDomainConfig("Frontend", listenAddress, true, false)
+
+	// XXX: Is this correct?
+	trustDomainCfgs = append(trustDomainCfgs, frontendServerTrustDomain)
+
+	// create a ClientConfig
+	clientConfig := &libtalek.ClientConfig{}
+	err = json.Unmarshal(cfgJson, clientConfig)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+
+	// set the frontend address, trust domains
+	clientConfig.FrontendAddr = listenAddress
+	clientConfig.TrustDomains = trustDomainCfgs
+
+	// serialize clientConfig to .json
+	clientConfigBytes, err := json.MarshalIndent(&clientConfig, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+	fmt.Printf("serialized clientConfig: '%v'\n", string(clientConfigBytes))
+
+	// write client config to disk
+	err = ioutil.WriteFile(clientConfigPath, clientConfigBytes, 0660)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+
+	// flag -g toggles writing a config and exiting
+	if configOnly {
+		os.Exit(0)
+	}
+
+	// create a FrontendServer Config
 	serverConfig := &server.Config{
-		// XXX: this should be learned from PKI too
-		Config:           commonCfgs[0],
-		TrustDomain:      &tCommon.TrustDomainConfig{},
-		TrustDomainIndex: 0,
+		Config:      commonCfg,
+		TrustDomain: frontendServerTrustDomain,
 	}
 
 	// start the frontend server
@@ -230,35 +277,6 @@ func main() {
 	f.Frontend.Verbose = true // *verbose
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, os.Interrupt)
-
-	// generate a client configuration to use with talekclient
-	// XXX : find out how to populate Config
-	commoncfg := commonCfgs[0]
-	clientConfig := libtalek.ClientConfig{Config: &tCommon.Config{}, FrontendAddr: "127.0.0.1"+listen}
-	commonJson, err := json.Marshal(commoncfg)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(-1)
-	}
-	err = json.Unmarshal(commonJson, &clientConfig)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(-1)
-	}
-
-	// add trustdomain configs
-	clientConfig.TrustDomains = trustDomainCfgs
-	clientConfigBytes, err := json.Marshal(clientConfig)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(-1)
-	}
-
-	err = ioutil.WriteFile(clientConfigPath, clientConfigBytes, 0660)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(-1)
-	}
 
 	<-sigCh
 }
