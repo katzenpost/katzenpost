4d7ae5669b6296c488e165b9aaf60c3f113cf1a6
David Stainton <dstainton415@gmail.com>
2024-03-20 20:57:27 -0700

    Add http_proxy server plugin

---
+++ server_plugins/cbor_plugins/http_proxy/main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"path/filepath"
+
+	"github.com/fxamacker/cbor/v2"
+	"gopkg.in/op/go-logging.v1"
+
+	"github.com/katzenpost/katzenpost/core/log"
+	"github.com/katzenpost/katzenpost/server/cborplugin"
+)
+
+func main() {
+	var logLevel string
+	var logDir string
+	var destURL string
+
+	flag.StringVar(&logDir, "log_dir", "", "logging directory")
+	flag.StringVar(&logLevel, "log_level", "DEBUG", "logging level could be set to: DEBUG, INFO, NOTICE, WARNING, ERROR, CRITICAL")
+	flag.StringVar(&destURL, "dest_url", "", "destination URL for reverse proxying to")
+	flag.Parse()
+
+	if destURL == "" {
+		panic("proxy destination must be set")
+	}
+	myurl, err := url.Parse(destURL)
+	if err != nil {
+		panic(err)
+	}
+
+	// Ensure that the log directory exists.
+	s, err := os.Stat(logDir)
+	if os.IsNotExist(err) {
+		fmt.Printf("Log directory '%s' doesn't exist.", logDir)
+		os.Exit(1)
+	}
+	if !s.IsDir() {
+		fmt.Println("Log directory must actually be a directory.")
+		os.Exit(1)
+	}
+
+	// Log to a file.
+	logFile := path.Join(logDir, fmt.Sprintf("memspool.%d.log", os.Getpid()))
+	logBackend, err := log.New(logFile, logLevel, false)
+	if err != nil {
+		panic(err)
+	}
+	serverLog := logBackend.GetLogger("memspool_server")
+
+	// start service
+	tmpDir, err := os.MkdirTemp("", "memspool_server")
+	if err != nil {
+		panic(err)
+	}
+	socketFile := filepath.Join(tmpDir, fmt.Sprintf("%d.blockchain_proxy.socket", os.Getpid()))
+
+	var server *cborplugin.Server
+
+	h := New(serverLog, myurl)
+	server = cborplugin.NewServer(serverLog, socketFile, new(cborplugin.RequestFactory), h)
+
+	// emit socketFile to stdout, because this tells the mix server where to connect
+	fmt.Printf("%s\n", socketFile)
+
+	server.Accept()
+	server.Wait()
+
+	os.Remove(socketFile)
+}
+
+// Request is the type which is serialized into the cborplugin request payload.
+type Request struct {
+	// Payload contains the proxied http request
+	Payload []byte
+}
+
+type proxyRequestHandler struct {
+	log  *logging.Logger
+	dest *url.URL
+}
+
+func New(log *logging.Logger, dest *url.URL) *proxyRequestHandler {
+	return &proxyRequestHandler{
+		log:  log,
+		dest: dest,
+	}
+}
+
+// OnCommand processes a SpoolRequest and returns a SpoolResponse
+func (s *proxyRequestHandler) OnCommand(cmd cborplugin.Command) (cborplugin.Command, error) {
+	switch r := cmd.(type) {
+	case *cborplugin.Request:
+		// the padding bytes were not stripped because
+		// without parsing the start of Payload we wont
+		// know how long it is, so we will use a streaming
+		// decoder and simply return the first cbor object
+		// and then discard the decoder and buffer
+		req := &Request{}
+		dec := cbor.NewDecoder(bytes.NewReader(r.Payload))
+		err := dec.Decode(req)
+		if err != nil {
+			return nil, err
+		}
+
+		request, err := http.ReadRequest(bufio.NewReader(bytes.NewReader(req.Payload)))
+		if err != nil {
+			return nil, err
+		}
+		resp, err := http.DefaultClient.Do(request)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		return &cborplugin.Response{Payload: body}, nil
+	default:
+		s.log.Errorf("OnCommand called with unknown Command type")
+		return nil, errors.New("Invalid Command type")
+	}
+}
+
+// RegisterConsumer is required by our plugin system interface
+func (s *proxyRequestHandler) RegisterConsumer(svr *cborplugin.Server) {}
