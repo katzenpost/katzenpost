0bd5a475bb51c712960d5cfad7577ee3fe798d5e
David Stainton <dstainton415@gmail.com>
2024-05-13 17:04:40 -0700

    Add sphinx benchmarks for 0kn

--- core/sphinx/x25519_benchmark_test.go
+++ core/sphinx/x25519_benchmark_test.go
@@ -19,10 +19,76 @@ package sphinx
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
+	kemschemes "github.com/katzenpost/hpqc/kem/schemes"
+	"github.com/katzenpost/hpqc/nike/schemes"
 	ecdh "github.com/katzenpost/hpqc/nike/x25519"
 	"github.com/katzenpost/hpqc/rand"
+
+	"github.com/katzenpost/katzenpost/core/sphinx/geo"
 )
 
 func BenchmarkX25519SphinxUnwrap(b *testing.B) {
 	benchmarkSphinxUnwrap(b, ecdh.Scheme(rand.Reader))
+}
+
+func Benchmark0knNIKESphinxUnwrap(b *testing.B) {
+	const testPayload = "It is the stillest words that bring on the storm.  Thoughts that come on doves’ feet guide the world."
+
+	mynike := schemes.ByName("x25519")
+	g := geo.GeometryFromUserForwardPayloadLength(mynike, 30000, true, 5)
+
+	payload := make([]byte, g.ForwardPayloadLength)
+	copy(payload, testPayload)
+
+	sphinx := NewSphinx(g)
+	nodes, path := benchNewPathVector(g.NrHops, true, sphinx.nike)
+
+	pkt, err := sphinx.NewPacket(rand.Reader, path, payload)
+	require.NoError(b, err)
+	if len(pkt) != g.HeaderLength+g.PayloadTagLength+len(payload) {
+		panic("packet length mismatch")
+	}
+
+	for n := 0; n < b.N; n++ {
+		testPacket := make([]byte, len(pkt))
+		copy(testPacket, pkt)
+		_, _, _, err := sphinx.Unwrap(nodes[0].privateKey, testPacket)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func Benchmark0knKEMSphinxUnwrap(b *testing.B) {
+	const testPayload = "It is the stillest words that bring on the storm.  Thoughts that come on doves’ feet guide the world."
+
+	mykem := kemschemes.ByName("MLKEM768-X25519")
+	if mykem == nil {
+		panic("kem is nil")
+	}
+	g := geo.KEMGeometryFromUserForwardPayloadLength(mykem, 30000, true, 5)
+	payload := make([]byte, g.ForwardPayloadLength)
+	copy(payload, testPayload)
+
+	sphinx := NewKEMSphinx(mykem, g)
+
+	kemnodes, kempath := newBenchKEMPathVector(mykem, g.NrHops, true)
+	pkt, err := sphinx.newKEMPacket(rand.Reader, kempath, payload)
+	if err != nil {
+		panic("wtf")
+	}
+	if len(pkt) != g.HeaderLength+g.PayloadTagLength+len(payload) {
+		panic("wtf")
+	}
+
+	for n := 0; n < b.N; n++ {
+		testPacket := make([]byte, len(pkt))
+		copy(testPacket, pkt)
+		_, _, _, err := sphinx.Unwrap(kemnodes[0].privateKey, testPacket)
+		if err != nil {
+			panic("wtf")
+		}
+	}
 }
