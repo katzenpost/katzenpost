590ce93b49d7d159ad7a3f89adfbf187cc60adc5
David Stainton <dstainton415@gmail.com>
2024-05-15 21:16:51 -0700

    WIP

--- server/internal/decoy/decoy_test.go
+++ server/internal/decoy/decoy_test.go
@@ -227,8 +227,8 @@ func generateNodes(isGatewayNode, isServiceNode bool, num int, epoch uint64) ([]
 			IdentityKey: blob,
 			LinkKey:     linkKeyBlob,
 			MixKeys:     mixKeys,
-			Addresses: map[pki.Transport][]string{
-				pki.Transport("tcp4"): []string{fmt.Sprintf("127.0.0.1:%d", i+1)},
+			Addresses: map[string][]string{
+				"tcp4": []string{fmt.Sprintf("127.0.0.1:%d", i+1)},
 			},
 			Kaetzchen:     nil,
 			IsServiceNode: isServiceNode,
