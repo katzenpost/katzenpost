fe657c488427c8fef2bb2775d08231fe17469de6
David Stainton <dstainton415@gmail.com>
2024-10-19 14:30:03 -0700

    replica/cmd: Fixup main func

--- replica/cmd/replica/main.go
+++ replica/cmd/replica/main.go
@@ -12,7 +12,6 @@ import (
 	"syscall"
 
 	"github.com/carlmjohnson/versioninfo"
-	"github.com/linxGnu/grocksdb"
 
 	"github.com/katzenpost/katzenpost/core/compat"
 	"github.com/katzenpost/katzenpost/replica"
@@ -20,7 +19,7 @@ import (
 )
 
 func main() {
-	cfgFile := flag.String("f", "katzenpost.toml", "Path to the server config file.")
+	cfgFile := flag.String("f", "replica_server.toml", "Path to the replica server config file.")
 	genOnly := flag.Bool("g", false, "Generate the keys and exit immediately.")
 	version := flag.Bool("v", false, "Get version info.")
 
@@ -29,6 +28,9 @@ func main() {
 	if *version {
 		fmt.Printf("version is %s\n", versioninfo.Short())
 		return
+	}
+	if *cfgFile == "" {
+		panic("-f must specify a replica server config file location")
 	}
 
 	// Set the umask to something "paranoid".
@@ -46,7 +48,7 @@ func main() {
 
 	cfg, err := config.LoadFile(*cfgFile, *genOnly)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to load config file '%v': %v\n", *cfgFile, err)
+		fmt.Fprintf(os.Stderr, "Failed to load server config file '%v': %v\n", *cfgFile, err)
 		os.Exit(-1)
 	}
 
@@ -82,34 +84,4 @@ func main() {
 
 	// Wait for the server to explode or be terminated.
 	svr.Wait()
-}
-
-// XXX Fix me.
-func dbStuff() {
-
-	bbto := grocksdb.NewDefaultBlockBasedTableOptions()
-	bbto.SetBlockCache(grocksdb.NewLRUCache(3 << 30))
-
-	opts := grocksdb.NewDefaultOptions()
-	opts.SetBlockBasedTableFactory(bbto)
-	opts.SetCreateIfMissing(true)
-
-	db, err := grocksdb.OpenDb(opts, "1.db")
-	if err != nil {
-		panic(err)
-	}
-
-	//ro := grocksdb.NewDefaultReadOptions()
-	wo := grocksdb.NewDefaultWriteOptions()
-
-	fmt.Println("before db put")
-
-	err = db.Put(wo, []byte("foo"), []byte("bar"))
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("after db put")
-
-	db.Close()
 }
