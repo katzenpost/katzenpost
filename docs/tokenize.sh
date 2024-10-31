7cb9973fafe8d41efd46185577bdac250207f415
David Robinson <dwrob@oneeyedman.net>
2024-10-31 14:12:07 -0700

    Replacing with correct file

--- docs/tokenize.sh
+++ docs/tokenize.sh
@@ -2,92 +2,39 @@
 
 cd
 
-# test for existing Eclipse installation
+cp -r $HOME/eclipse $HOME/eclipse-temp
+cp -r $HOME/.p2 $HOME/.p2-temp
+cp -r $HOME/Echomix-workspace $HOME/Echomix-workspace-temp
 
-ECLIPSE_START=$HOME/eclipse/java-2024-09/eclipse/eclipse
-DIR1="${HOME}/eclipse"
-DIR2="${HOME}/.p2"
-DIR3="${HOME}/Echomix-workspace"
-FAILURE="n"
+# First tokenize the $GITPATH
 
-if [ -d $DIR1 ]; then
-    echo "Directory ${DIR1} from existing Eclipse installation found."
-    echo "Remove directories eclipse, .p2, and *-workspace and try again."
-    exit 1
-fi
-
-if [ -d $DIR2 ]; then
-    echo "Directory ${DIR2} from existing Eclipse installation found."
-    echo "Remove directories eclipse, .p2, and *-workspace and try again."
-    exit 1 
-fi
-
-if [ -d $DIR3 ]; then
-    echo "Directory ${DIR3} from existing Eclipse installation found."
-    echo "Remove directories eclipse, .p2, and *-workspace and try again."
-    exit 1 
-fi
-
-echo "What is the relative path, from your home directory, to your local katzenpost git repository?"
-echo "(e.g., git/katzenpost)"
-
-echo "Type the path:"
-
-read -r GITPATH
-
-echo
-
-if [ -d $GITPATH ]; then
-
-echo "Git path is valid, proceeding...."
-
-else
-
-echo "Invalid git path. Aborting. Determine the correct path and try again."
-
-exit 1
-
-fi
-
-gunzip -dc $HOME/collaborative_eclipse.tar.gz | tar xvf -
-
-# First set the git path
-
-find $HOME/Echomix-workspace-temp/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.egit.core.prefs -type f -exec sed -i "s|AAAtokenZZZ|${HOME}/${GITPATH}/|g" {} \;
-find $HOME/Echomix-workspace-temp/.metadata/.plugins/org.eclipse.ui.ide/dialog_settings.xml -type f -exec sed -i "s|AAAtokenZZZ|${HOME}/${GITPATH}/|g" {} \;
+find $HOME/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.egit.core.prefs -type f -exec sed -i "s|${HOME}\/git\/katzenpost|AAAtokenZZZ|g" {} \;
+find $HOME/.metadata/.plugins/org.eclipse.ui.ide/dialog_settings.xml -type f -exec sed -i "s|${HOME}\/git\/katzenpost|AAAtokenZZZ|g" {} \;
 
 # /home/$USER (file content)
 
-find $HOME/eclipse-temp -type f -exec sed -i "s|\/home\/AAAtokenZZZ|${HOME}|g" {} \;
-find $HOME/.p2-temp -type f -exec sed -i "s|\/home\/AAAtokenZZZ|${HOME}|g" {} \;
-find $HOME/Echomix-workspace-temp -type f -exec sed -i "s|\/home\/AAAtokenZZZ|${HOME}|g" {} \;
+find $HOME/eclipse-temp -type f -exec sed -i "s|${HOME}|\/home\/AAAtokenZZZ|g" {} \;
+find $HOME/.p2-temp -type f -exec sed -i "s|${HOME}|\/home\/AAAtokenZZZ|g" {} \;
+find $HOME/Echomix-workspace-temp -type f -exec sed -i "s|${HOME}|\/home\/AAAtokenZZZ|g" {} \;
 
 # _home_$USER (file content)
 
-find $HOME/eclipse-temp -type f -exec sed -i "s|_home_AAAtokenZZZ|_home_${USER}|g" {} \;
-find $HOME/.p2-temp -type f -exec sed -i "s|_home_AAAtokenZZZ|_home_${USER}|g" {} \;
+$HOME/find eclipse-temp -type f -exec sed -i "s|_home_${USER}|_home_AAAtokenZZZ|g" {} \;
+$HOME/find .p2-temp -type f -exec sed -i "s|_home_${USER}|_home_AAAtokenZZZ|g" {} \;
 
 # _home_$USER (directory names)
 
-mv "${HOME}/.p2-temp/org.eclipse.equinox.p2.engine/profileRegistry/_home_AAAtokenZZZ_eclipse_~_eclipse_java-2024-09_eclipse.profile" "${HOME}/.p2-temp/org.eclipse.equinox.p2.engine/profileRegistry/_home_${USER}_eclipse_~_eclipse_java-2024-09_eclipse.profile"
-mv "${HOME}/.p2-temp/org.eclipse.equinox.p2.engine/profileRegistry/_home_AAAtokenZZZ_eclipse_java-2024-09_eclipse.profile" "${HOME}/.p2-temp/org.eclipse.equinox.p2.engine/profileRegistry/_home_${USER}_eclipse_java-2024-09_eclipse.profile"
+# $USER@skamander:~$ find .p2 -name "_home_${USER}*"
+# .p2/org.eclipse.equinox.p2.engine/profileRegistry/_home_$USER_eclipse_~_eclipse_java-2024-09_eclipse.profile
+# .p2/org.eclipse.equinox.p2.engine/profileRegistry/_home_$USER_eclipse_java-2024-09_eclipse.profile
 
-mv $HOME/eclipse-temp $HOME/eclipse
-mv $HOME/.p2-temp $HOME/.p2
-mv $HOME/Echomix-workspace-temp $HOME/Echomix-workspace
+mv "${HOME}/.p2-temp/org.eclipse.equinox.p2.engine/profileRegistry/_home_${USER}_eclipse_~_eclipse_java-2024-09_eclipse.profile" "${HOME}/.p2-temp/org.eclipse.equinox.p2.engine/profileRegistry/_home_AAAtokenZZZ_eclipse_~_eclipse_java-2024-09_eclipse.profile"
+mv "${HOME}/.p2-temp/org.eclipse.equinox.p2.engine/profileRegistry/_home_${USER}_eclipse_java-2024-09_eclipse.profile" "${HOME}/.p2-temp/org.eclipse.equinox.p2.engine/profileRegistry/_home_AAAtokenZZZ_eclipse_java-2024-09_eclipse.profile"
 
-echo 
-echo "Installation complete. Start Eclipse?"
-echo
-echo "y/n:"
+# Why is this even there, recording random system information?
 
-read $START
+rm -r $HOME/Echomix-workspace-temp/.metadata/.plugins/org.eclipse.core.resources/.history/
 
-if [[ $START == "y" ]]; then
+tar -czf $HOME/collaborative_eclipse.tar.gz $HOME/eclipse-temp $HOME/.p2-temp $HOME/Echomix-workspace-temp
 
-$ECLIPSE_START
-
-exit 1
-
-fi
-
+rm -r $HOME/eclipse-temp $HOME/.p2-temp $HOME/Echomix-workspace-temp
