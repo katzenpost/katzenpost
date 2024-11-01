e91595852c85481767b2365727d96393f412dd72
David Robinson <dwrob@oneeyedman.net>
2024-10-31 21:03:34 -0700

    Script work

--- docs/tokenize.sh
+++ docs/tokenize.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/bash
 
+echo
+echo "This script purges user-specific strings in the directories and files"
+echo "of the local existing Eclipse installation and inserts tokens in their place."
+echo
+
 cd
+
+echo "* preparing temp files"
 
 cp -r $HOME/eclipse $HOME/eclipse-temp
 cp -r $HOME/.p2 $HOME/.p2-temp
 cp -r $HOME/Echomix-workspace $HOME/Echomix-workspace-temp
 
+echo "* tokenizing git path"
+
 # First tokenize the $GITPATH
 
 find $HOME/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.egit.core.prefs -type f -exec sed -i "s|${HOME}\/git\/katzenpost|AAAtokenZZZ|g" {} \;
 find $HOME/.metadata/.plugins/org.eclipse.ui.ide/dialog_settings.xml -type f -exec sed -i "s|${HOME}\/git\/katzenpost|AAAtokenZZZ|g" {} \;
+
+echo "* tokenizing file contents"
 
 # /home/$USER (file content)
 
@@ -19,8 +30,10 @@ find $HOME/Echomix-workspace-temp -type f -exec sed -i "s|${HOME}|\/home\/AAAtok
 
 # _home_$USER (file content)
 
-$HOME/find eclipse-temp -type f -exec sed -i "s|_home_${USER}|_home_AAAtokenZZZ|g" {} \;
-$HOME/find .p2-temp -type f -exec sed -i "s|_home_${USER}|_home_AAAtokenZZZ|g" {} \;
+find $HOME/eclipse-temp -type f -exec sed -i "s|_home_${USER}|_home_AAAtokenZZZ|g" {} \;
+find $HOME/.p2-temp -type f -exec sed -i "s|_home_${USER}|_home_AAAtokenZZZ|g" {} \;
+
+echo "* tokenizing directory names"
 
 # _home_$USER (directory names)
 
@@ -33,8 +46,17 @@ mv "${HOME}/.p2-temp/org.eclipse.equinox.p2.engine/profileRegistry/_home_${USER}
 
 # Why is this even there, recording random system information?
 
+echo "* removing history directory, if any"
+
 rm -r $HOME/Echomix-workspace-temp/.metadata/.plugins/org.eclipse.core.resources/.history/
+
+echo "* tarring, zipping"
 
 tar -czf $HOME/collaborative_eclipse.tar.gz $HOME/eclipse-temp $HOME/.p2-temp $HOME/Echomix-workspace-temp
 
+echo "* cleaning up temp files"
+
 rm -r $HOME/eclipse-temp $HOME/.p2-temp $HOME/Echomix-workspace-temp
+
+echo
+echo "DONE! Compressed archive collaborative_eclipse.tar.gz is ready for expost."
