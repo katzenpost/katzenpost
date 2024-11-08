b77edc842d7b04fd3f06ee854990b0535961031d
David Stainton <dstainton415@gmail.com>
2024-11-07 20:59:24 -0800

    replica/common: remove old slower Shard function

--- replica/common/shard.go
+++ replica/common/shard.go
@@ -40,32 +40,6 @@ type serverDesc struct {
 	hash *[32]byte
 }
 
-// Shard implements our consistent hashing scheme for the sharded pigeonhole database.
-// It returns the first K`th entries from our sorted list of hashes
-// where each hash is the hash of the boxID concatenated with the server ID key.
-func Shard(boxID *[32]byte, serverIdKeys [][]byte) [][]byte {
-	servers := make([]*serverDesc, 0, len(serverIdKeys))
-	for _, key := range serverIdKeys {
-		hash := blake2b.Sum256(append(key, boxID[:]...))
-		d := &serverDesc{
-			key:  key,
-			hash: &hash,
-		}
-		servers = append(servers, d)
-	}
-
-	slices.SortFunc(servers, func(a, b *serverDesc) int {
-		return slices.Compare(a.hash[:], b.hash[:])
-	})
-
-	result := make([][]byte, 0, K)
-	for i := 0; i < K; i++ {
-		result = append(result, servers[i].key)
-	}
-
-	return result
-}
-
 // Shard2 implements our consistent hashing scheme for the sharded pigeonhole database
 // where K is fixed to 2.
 // It returns the first K`th entries from our sorted list of hashes
