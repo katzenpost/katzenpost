76be79f52ee71a81aae29db96c02bfa8fb5806c5
David Stainton <dstainton415@gmail.com>
2024-11-09 17:39:57 -0800

    replica: add more tests

--- replica/outgoing_conn_test.go
+++ replica/outgoing_conn_test.go
@@ -11,14 +11,18 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/katzenpost/hpqc/hash"
 	kemschemes "github.com/katzenpost/hpqc/kem/schemes"
 	nikeschemes "github.com/katzenpost/hpqc/nike/schemes"
 	ecdh "github.com/katzenpost/hpqc/nike/x25519"
 	"github.com/katzenpost/hpqc/rand"
 	signschemes "github.com/katzenpost/hpqc/sign/schemes"
 
+	authconfig "github.com/katzenpost/katzenpost/authority/voting/server/config"
+	"github.com/katzenpost/katzenpost/core/epochtime"
 	"github.com/katzenpost/katzenpost/core/pki"
 	"github.com/katzenpost/katzenpost/core/sphinx/geo"
+	"github.com/katzenpost/katzenpost/core/wire"
 	"github.com/katzenpost/katzenpost/replica/config"
 )
 
@@ -37,11 +41,33 @@ func TestOutgoingConn(t *testing.T) {
 	linkScheme := kemschemes.ByName("x25519")
 	replicaScheme := nikeschemes.ByName("x25519")
 
+	idpubkey, _, err := pkiScheme.GenerateKey()
+	require.NoError(t, err)
+	idpubkeyblob, err := idpubkey.MarshalBinary()
+	require.NoError(t, err)
+
+	dstReplicaDesc := generateReplica(t, pkiScheme, linkScheme, replicaScheme)
+	linkpubkey, err := linkScheme.UnmarshalBinaryPublicKey(dstReplicaDesc.LinkKey)
+	require.NoError(t, err)
+
 	pk, _, err := pkiScheme.GenerateKey()
 	require.NoError(t, err)
 
 	cfg := &config.Config{
-		PKI:        &config.PKI{},
+		PKI: &config.PKI{
+			Voting: &config.Voting{
+				Authorities: []*authconfig.Authority{
+					&authconfig.Authority{
+						Identifier:         "dirauth1",
+						IdentityPublicKey:  idpubkey,
+						PKISignatureScheme: pkiScheme.Name(),
+						LinkPublicKey:      linkpubkey,
+						WireKEMScheme:      linkScheme.Name(),
+						Addresses:          []string{"tcp://127.0.0.1:1234"},
+					},
+				},
+			},
+		},
 		Identifier: "replica1",
 		Logging: &config.Logging{
 			Disable: false,
@@ -70,11 +96,44 @@ func TestOutgoingConn(t *testing.T) {
 	s.pkiWorker.server = s
 	s.connector = newMockConnector(s)
 
+	epoch, _, _ := epochtime.Now()
+	s.pkiWorker.lock.Lock()
+	s.pkiWorker.docs[epoch] = &pki.Document{
+		Epoch: epoch,
+		ServiceNodes: []*pki.MixDescriptor{
+			&pki.MixDescriptor{
+				Name:        "servicenode1",
+				Epoch:       epoch,
+				IdentityKey: idpubkeyblob,
+				LinkKey:     dstReplicaDesc.LinkKey,
+			},
+		},
+	}
+	s.pkiWorker.docs[epoch] = &pki.Document{
+		Epoch: epoch,
+		StorageReplicas: []*pki.ReplicaDescriptor{
+			dstReplicaDesc,
+		},
+	}
+	adID := hash.Sum256(dstReplicaDesc.IdentityKey)
+	s.pkiWorker.replicas.Replace(map[[32]byte]*pki.ReplicaDescriptor{adID: dstReplicaDesc})
+
+	s.pkiWorker.lock.Unlock()
+	s.pkiWorker.server = s
+	s.pkiWorker.log = s.LogBackend().GetLogger("pki")
+
 	err = s.initLogging()
 	require.NoError(t, err)
 
-	dstReplicaDesc := generateReplica(t, pkiScheme, linkScheme, replicaScheme)
+	outConn := newOutgoingConn(s.connector, dstReplicaDesc, geo, linkScheme)
+	creds := &wire.PeerCredentials{}
+	ok := outConn.IsPeerValid(creds)
+	require.False(t, ok)
 
-	//outConn := newOutgoingConn(s.connector, dstReplicaDesc, geo, linkScheme)
-	newOutgoingConn(s.connector, dstReplicaDesc, geo, linkScheme)
+	creds = &wire.PeerCredentials{
+		AdditionalData: adID[:],
+		PublicKey:      linkpubkey,
+	}
+	ok = outConn.IsPeerValid(creds)
+	require.True(t, ok)
 }
