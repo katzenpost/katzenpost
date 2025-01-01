655ec0cb4b371146f2c0fe646ec7cdeb3824cbb3
David Stainton <dstainton415@gmail.com>
2025-01-01 15:06:38 -0800

    mix server: advertize courier via capability

--- server/internal/service/service.go
+++ server/internal/service/service.go
@@ -71,7 +71,7 @@ func (p *serviceNode) KaetzchenForPKI() (map[string]map[string]interface{}, map[
 	kaetzchenAdvertizedData := make(map[string]map[string]interface{})
 	cfg := p.glue.Config()
 	for _, v := range cfg.ServiceNode.CBORPluginKaetzchen {
-		kaetzchenAdvertizedData[v.Endpoint] = v.PKIAdvertizedData
+		kaetzchenAdvertizedData[v.Capability] = v.PKIAdvertizedData
 	}
 
 	return kaetzchenAdvertizedData, merged, nil
