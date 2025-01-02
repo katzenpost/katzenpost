f00070ee8a421e1c0bbeb1ccdb7d90bd82b4bfa3
David Stainton <dstainton415@gmail.com>
2025-01-02 14:07:26 -0800

    client2/common: add debug prints

--- client2/common/common.go
+++ client2/common/common.go
@@ -4,6 +4,8 @@
 package common
 
 import (
+	"fmt"
+
 	cpki "github.com/katzenpost/katzenpost/core/pki"
 )
 
@@ -17,12 +19,17 @@ type ServiceDescriptor struct {
 
 // FindServices is a helper function for finding Gateway-side services in the PKI document.
 func FindServices(capability string, doc *cpki.Document) []*ServiceDescriptor {
+	fmt.Printf("FindServices: capability -> %s\n", capability)
+	fmt.Printf("PKI DOC: %s\n", doc.String())
+
 	if doc == nil {
 		panic("pki doc is nil")
 	}
 	services := []*ServiceDescriptor{}
+	fmt.Printf("num service nodes %d\n", len(doc.ServiceNodes))
 	for _, provider := range doc.ServiceNodes {
 		for cap := range provider.Kaetzchen {
+			fmt.Printf("comparing caps %s and %s\n", cap, capability)
 			if cap == capability {
 				serviceID := &ServiceDescriptor{
 					RecipientQueueID: []byte(provider.Kaetzchen[cap]["endpoint"].(string)),
