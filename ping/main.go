7d9311a5973498cba080a1363cc0f4adbcb1f291
David Stainton <dstainton415@gmail.com>
2025-06-06 13:10:14 -0700

    ping: refactor main

--- ping/main.go
+++ ping/main.go
@@ -19,12 +19,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/carlmjohnson/versioninfo"
-
 	"github.com/katzenpost/hpqc/rand"
-
 	"github.com/katzenpost/katzenpost/client2"
 	"github.com/katzenpost/katzenpost/client2/config"
 	"github.com/katzenpost/katzenpost/client2/thin"
@@ -43,14 +42,9 @@ func randUser() string {
 	return fmt.Sprintf("%x", user[:])
 }
 
-func main() {
-	var configFile string
-	var service string
-	var count int
+// parseFlags handles command line flag parsing and validation
+func parseFlags() (configFile, service string, count, concurrency int, printDiff, thinClientOnly bool) {
 	var timeout int
-	var concurrency int
-	var printDiff bool
-	var thinClientOnly bool
 	flag.StringVar(&configFile, "c", "", "configuration file")
 	flag.StringVar(&service, "s", "", "service name")
 	flag.IntVar(&count, "n", 5, "count")
@@ -63,69 +57,93 @@ func main() {
 
 	if *version {
 		fmt.Printf("version is %s\n", versioninfo.Short())
-		return
+		os.Exit(0)
 	}
 
 	if service == "" {
 		panic("must specify service name with -s")
 	}
 
-	var thinClient *thin.ThinClient
-	var daemon *client2.Daemon
+	return configFile, service, count, concurrency, printDiff, thinClientOnly
+}
 
+// initializeClient sets up either thin client or full daemon mode
+func initializeClient(configFile string, thinClientOnly bool) (*thin.ThinClient, *client2.Daemon) {
 	if thinClientOnly {
-		// Thin client mode: connect to existing daemon
-		cfg, err := thin.LoadFile(configFile)
-		if err != nil {
-			panic(fmt.Errorf("failed to open thin client config: %s", err))
-		}
+		return initializeThinClient(configFile), nil
+	}
+	return initializeFullClient(configFile)
+}
 
-		logging := &config.Logging{
-			Disable: false,
-			File:    "",
-			Level:   "DEBUG",
-		}
-
-		thinClient = thin.NewThinClient(cfg, logging)
-		err = thinClient.Dial()
-		if err != nil {
-			panic(fmt.Errorf("failed to connect to daemon: %s", err))
-		}
-	} else {
-		// Full mode: start own daemon
-		cfg, err := config.LoadFile(configFile)
-		if err != nil {
-			panic(fmt.Errorf("failed to open config: %s", err))
-		}
-
-		// create a client and connect to the mixnet Gateway
-		daemon, err = client2.NewDaemon(cfg)
-		if err != nil {
-			panic(err)
-		}
-		err = daemon.Start()
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println("Sleeping for 3 seconds to let the client daemon startup...")
-		time.Sleep(time.Second * 3)
-
-		thinClient = thin.NewThinClient(thin.FromConfig(cfg), cfg.Logging)
-		err = thinClient.Dial()
-		if err != nil {
-			panic(err)
-		}
+// initializeThinClient sets up thin client mode
+func initializeThinClient(configFile string) *thin.ThinClient {
+	cfg, err := thin.LoadFile(configFile)
+	if err != nil {
+		panic(fmt.Errorf("failed to open thin client config: %s", err))
 	}
 
+	logging := &config.Logging{
+		Disable: false,
+		File:    "",
+		Level:   "DEBUG",
+	}
+
+	thinClient := thin.NewThinClient(cfg, logging)
+	err = thinClient.Dial()
+	if err != nil {
+		panic(fmt.Errorf("failed to connect to daemon: %s", err))
+	}
+	return thinClient
+}
+
+// initializeFullClient sets up full daemon mode
+func initializeFullClient(configFile string) (*thin.ThinClient, *client2.Daemon) {
+	cfg, err := config.LoadFile(configFile)
+	if err != nil {
+		panic(fmt.Errorf("failed to open config: %s", err))
+	}
+
+	// create a client and connect to the mixnet Gateway
+	daemon, err := client2.NewDaemon(cfg)
+	if err != nil {
+		panic(err)
+	}
+	err = daemon.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Sleeping for 3 seconds to let the client daemon startup...")
+	time.Sleep(time.Second * 3)
+
+	thinClient := thin.NewThinClient(thin.FromConfig(cfg), cfg.Logging)
+	err = thinClient.Dial()
+	if err != nil {
+		panic(err)
+	}
+	return thinClient, daemon
+}
+
+// executePing performs the ping operation
+func executePing(thinClient *thin.ThinClient, service string, count, concurrency int, printDiff bool) {
 	desc, err := thinClient.GetService(service)
 	if err != nil {
 		panic(err)
 	}
 
 	sendPings(thinClient, desc, count, concurrency, printDiff)
+}
 
+// cleanup handles daemon shutdown
+func cleanup(daemon *client2.Daemon) {
 	if daemon != nil {
 		daemon.Shutdown()
 	}
+}
+
+func main() {
+	configFile, service, count, concurrency, printDiff, thinClientOnly := parseFlags()
+	thinClient, daemon := initializeClient(configFile, thinClientOnly)
+	executePing(thinClient, service, count, concurrency, printDiff)
+	cleanup(daemon)
 }
