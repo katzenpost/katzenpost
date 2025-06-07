0aed7ab5fd3d1d510e59b95e0b1a544e73582cfb
David Stainton <dstainton415@gmail.com>
2025-06-06 23:23:55 -0700

    restore stream to main stream

--- stream/buffered_test.go
+++ stream/buffered_test.go
@@ -201,8 +201,7 @@ func TestBufferedStream(t *testing.T) {
 
 func TestLossyStream(t *testing.T) {
 	require := require.New(t)
-	// Use a lower loss rate to make the test more reliable
-	trans := NewLossyMockTransport(0.05) // 5% loss instead of 10%
+	trans := NewLossyMockTransport(0.1)
 	a, b := newStreams(trans)
 	a.Start()
 	b.Start()
@@ -210,18 +209,12 @@ func TestLossyStream(t *testing.T) {
 	sent := make([]*msg, 0)
 	recv := make([]*msg, 0)
 	enc := cbor.NewEncoder(a)
-
-	// Use fewer messages to reduce test time
-	testEntries := 5
-
 	wg := new(sync.WaitGroup)
 	wg.Add(2)
-
 	// write data to receiver
 	go func() {
-		defer wg.Done()
-		for i := 0; i < testEntries; i++ {
-			hello := &msg{Num: i, Name: "Tester", Payload: make([]byte, 512)} // Smaller payload
+		for i := 0; i < numEntries; i++ {
+			hello := &msg{Num: i, Name: "Tester", Payload: make([]byte, 1024)}
 			sent = append(sent, hello)
 			err := enc.Encode(hello)
 			require.NoError(err)
@@ -229,66 +222,33 @@ func TestLossyStream(t *testing.T) {
 		}
 		a.Sync()
 		a.Close()
+		wg.Done()
 	}()
-
 	bs := BufferedStream{Stream: b}
 	bs.Start()
-
-	// read data with timeout to prevent hanging
+	// read data
 	go func() {
-		defer wg.Done()
-		defer b.Close()
-
-		// Give more time for retransmissions to work
-		overallTimeout := time.After(60 * time.Second)
-
-		for i := 0; i < testEntries; i++ {
+		for i := 0; i < numEntries; i++ {
 			r := new(msg)
-
-			// Per-message timeout to allow for retransmissions
-			messageTimeout := time.After(15 * time.Second)
-
-			// Use CBORDecodeAsync which returns a channel and is cancellable
-			resultCh := bs.CBORDecodeAsync(r)
-
-			select {
-			case result := <-resultCh:
-				if err, ok := result.(error); ok {
-					if err == io.EOF {
-						t.Logf("got EOF after receiving %d messages", len(recv))
-						return
-					}
-					t.Logf("decode error: %v", err)
-					return
-				}
-				recv = append(recv, r)
-				t.Logf("recv data %d", i)
-			case <-messageTimeout:
-				t.Logf("timeout waiting for message %d after receiving %d messages", i, len(recv))
-				return
-			case <-overallTimeout:
-				t.Logf("overall timeout after receiving %d messages", len(recv))
-				return
+			err := bs.CBORDecode(r)
+			if err == io.EOF {
+				require.Equal(i, numEntries-1)
+				break
 			}
+			require.NoError(err)
+			recv = append(recv, r)
+			t.Logf("recv data %d", i)
 		}
+		b.Close()
+		wg.Done()
 	}()
-
 	wg.Wait()
-	a.Halt()
-	bs.Halt()
-
-	// With lossy transport, we expect some messages might be lost
-	// The test passes if we receive at least 60% of messages (3 out of 5)
-	minExpected := int(float64(testEntries) * 0.6)
-	require.GreaterOrEqual(len(recv), minExpected, "Should receive at least %d messages", minExpected)
-	require.LessOrEqual(len(recv), len(sent), "Should not receive more messages than sent")
-
-	// Verify that received messages are in correct order and match sent messages
-	for i := 0; i < len(recv); i++ {
-		require.Equal(sent[recv[i].Num], recv[i], "Message content should match")
+	require.Equal(len(sent), len(recv))
+	for i := 0; i < numEntries; i++ {
+		require.Equal(sent[i], recv[i])
 	}
-
-	t.Logf("Test completed: sent %d messages, received %d messages", len(sent), len(recv))
+	a.Halt()
+	b.Halt()
 }
 
 func init() {
