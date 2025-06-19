5b0ddad329931e08447f1c8163b3128511d57507
David Stainton <dstainton415@gmail.com>
2025-06-19 12:14:41 -0700

    refactor: reduce cognitive complexity in katzencopy main function
    Extract nested logic into focused functions to reduce complexity from 49 to <15:
    - Split file send/receive operations into separate functions
    - Extract validation and acknowledgment logic
    - Separate initiator and receiver control flows

--- cmd/katzencopy/main.go
+++ cmd/katzencopy/main.go
@@ -51,113 +51,165 @@ func getSession() (*client.Session, error) {
 	}
 }
 
+func validateArgs(secret, inFile, outFile string) {
+	if secret == "" && inFile == "" {
+		fmt.Println("Must specify input file")
+		os.Exit(-1)
+	}
+	if secret != "" && outFile == "" {
+		fmt.Println("Must specify output file")
+		os.Exit(-1)
+	}
+}
+
+func sendLengthPrefix(st *stream.Stream, length uint64) {
+	lengthprefix := make([]byte, 8)
+	binary.BigEndian.PutUint64(lengthprefix, length)
+	for {
+		_, err := st.Write(lengthprefix)
+		if err == nil {
+			break
+		}
+	}
+}
+
+func sendFile(st *stream.Stream, filename string) error {
+	fi, err := os.Stat(filename)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	sendLengthPrefix(st, uint64(fi.Size()))
+
+	total := int64(0)
+	for total < fi.Size() {
+		limited := io.LimitReader(f, progressChunkSize)
+		n, err := io.Copy(st, limited)
+		total += n
+		if err == io.EOF {
+			return fmt.Errorf("unexpected EOF of limitReader")
+		}
+		fmt.Fprintf(os.Stderr, "\rWrite(%d/%d)", total, fi.Size())
+	}
+	return nil
+}
+
+func waitForAcknowledgment(st *stream.Stream) {
+	fmt.Fprintln(os.Stderr, "\nTransfer completed, waiting for peer to close stream")
+	for {
+		_, err := io.ReadAll(st)
+		if err == nil || err == io.EOF {
+			break
+		}
+	}
+	fmt.Fprintln(os.Stderr, "\nTransfer acknowledged, shutting down")
+}
+
+func readLengthPrefix(st *stream.Stream) (uint64, error) {
+	lengthprefix := make([]byte, 8)
+	n := 0
+	for n < 8 {
+		bytesRead, err := st.Read(lengthprefix[n:])
+		n += bytesRead
+		if err != nil {
+			continue
+		}
+	}
+	return binary.BigEndian.Uint64(lengthprefix), nil
+}
+
+func receiveFile(st *stream.Stream, filename string) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	payloadlen, err := readLengthPrefix(st)
+	if err != nil {
+		return err
+	}
+
+	total := uint64(0)
+	for total < payloadlen {
+		limitsize := int64(progressChunkSize)
+		if total+progressChunkSize > payloadlen {
+			limitsize = int64(payloadlen - total)
+		}
+
+		limited := io.LimitReader(st, limitsize)
+		nn, err := io.Copy(f, limited)
+		total += uint64(nn)
+
+		if err == os.ErrDeadlineExceeded {
+			continue
+		}
+		if err != nil {
+			return err
+		}
+
+		fmt.Fprintf(os.Stderr, "\rRead(%d/%d)", total, payloadlen)
+	}
+
+	fmt.Fprintln(os.Stderr, "\nTransfer completed, closing stream")
+	return nil
+}
+
+func runAsInitiator(s *client.Session, inFile string) error {
+	st := stream.NewStream(s)
+	secret := st.RemoteAddr().String()
+	fmt.Println("KatzenCat secret:", secret)
+
+	if err := sendFile(st, inFile); err != nil {
+		return err
+	}
+
+	waitForAcknowledgment(st)
+	st.Close()
+	return nil
+}
+
+func runAsReceiver(s *client.Session, secret, outFile string) error {
+	st, err := stream.DialDuplex(s, "", secret)
+	if err != nil {
+		return err
+	}
+	defer st.Close()
+
+	return receiveFile(st, outFile)
+}
+
 func main() {
 	secret := flag.String("s", "", "Secret given by initiator, or empty if initiating")
 	flag.Parse()
 
-	if *secret == "" && *inFile == "" {
-		fmt.Println("Must specify input file")
-		os.Exit(-1)
-	}
-	if *secret != "" && *outFile == "" {
-		fmt.Println("Must specify output file")
-		os.Exit(-1)
-	}
+	validateArgs(*secret, *inFile, *outFile)
 
 	s, err := getSession()
 	if err != nil {
 		panic(err)
 	}
+	defer func() {
+		// it seems that messages may get lost in the send queue if exit happens immediately after Close()
+		<-time.After(10 * time.Second)
+		s.Shutdown()
+		s.Wait()
+	}()
 
-	var st *stream.Stream
-	isinitiator := *secret == ""
-	if *secret == "" {
-		st = stream.NewStream(s)
-		*secret = st.RemoteAddr().String()
-		fmt.Println("KatzenCat secret:", *secret)
-	}
-	if isinitiator {
-		fi, err := os.Stat(*inFile)
-		if err != nil {
-			panic(err)
-		}
-		f, err := os.Open(*inFile)
-		if err != nil {
-			panic(err)
-		}
-		lengthprefix := make([]byte, 8)
-		binary.BigEndian.PutUint64(lengthprefix, uint64(fi.Size()))
-		for {
-			_, err := st.Write(lengthprefix)
-			if err == nil {
-				break
-			}
-		}
-		total := int64(0)
-		for total < fi.Size() {
-			limited := io.LimitReader(f, progressChunkSize)
-			n, err := io.Copy(st, limited)
-			total += n
-			if err == io.EOF {
-				panic("wtf, EOF of limitReader")
-			} else {
-				fmt.Fprintf(os.Stderr, "\rWrite(%d/%d)", total, fi.Size())
-				continue
-			}
-		}
-		fmt.Fprintln(os.Stderr, "\nTransfer completed, waiting for peer to close stream")
-		// try to read a response from the client until defaultTimeout, and log status
-		for {
-			_, err := io.ReadAll(st)
-			if err == nil || err == io.EOF {
-				break
-			}
-		}
-		// Teardown the sender side stream
-		fmt.Fprintln(os.Stderr, "\nTransfer acknowledged, shutting down")
-		st.Close()
+	isInitiator := *secret == ""
+	if isInitiator {
+		err = runAsInitiator(s, *inFile)
 	} else {
-		st, err := stream.DialDuplex(s, "", *secret)
-		if err != nil {
-			panic(err)
-		}
-		f, err := os.Create(*outFile)
-		if err != nil {
-			panic(err)
-		}
-		n := 0
-		lengthprefix := make([]byte, 8)
-		for n < 8 {
-			n, err = st.Read(lengthprefix[n:])
-			if err != nil {
-				continue
-			}
-		}
-		payloadlen := binary.BigEndian.Uint64(lengthprefix)
-		total := uint64(0)
-		for total < payloadlen {
-			limitsize := int64(progressChunkSize)
-			if total+progressChunkSize > payloadlen {
-				limitsize = int64(payloadlen - total)
-			}
-			limited := io.LimitReader(st, int64(limitsize))
-			nn, err := io.Copy(f, limited)
-			total += uint64(nn)
-			if err == os.ErrDeadlineExceeded {
-				continue
-			}
-			if err != nil {
-				panic(err)
-			}
-
-			fmt.Fprintf(os.Stderr, "\rRead(%d/%d)", total, payloadlen)
-		}
-		fmt.Fprintln(os.Stderr, "\nTransfer completed, closing stream")
-		st.Close()
-		f.Close()
+		err = runAsReceiver(s, *secret, *outFile)
 	}
-	// it seems that messages may get lost in the send queue if exit happens immediately after Close()
-	<-time.After(10 * time.Second)
-	s.Shutdown()
-	s.Wait()
+
+	if err != nil {
+		panic(err)
+	}
 }
