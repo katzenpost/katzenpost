9a53ca7236a11bbd53675e0c47be205a14777a0a
David Stainton <dstainton415@gmail.com>
2025-07-16 12:58:16 -0700

    common: Remove stupid test

--- common/exponential_test.go
+++ common/exponential_test.go
@@ -40,28 +40,3 @@ func TestExponential(t *testing.T) {
 	require.Equal(t, fired, 3)
 	require.Equal(t, failed, 0)
 }
-
-func NoTestExponentialAverage(t *testing.T) {
-	e := NewExpDist()
-
-	averageMsec := 100
-	maxMsec := 300
-
-	e.UpdateRate(uint64(averageMsec), uint64(maxMsec))
-	e.UpdateConnectionStatus(true)
-
-	numPkts := 100
-	base := time.Time{}
-	var duration time.Duration
-	for i := 0; i < numPkts; i++ {
-		then := time.Now()
-		<-e.OutCh()
-		timeSince := time.Since(then)
-		t.Logf("timeSince: %v", timeSince)
-		duration += timeSince
-		require.Less(t, timeSince, time.Duration(maxMsec+20)*time.Millisecond) // clamping to maxMsec slops over maxMsec < 1ms
-	}
-
-	// require that the average over 100 samples be within 2 seconds (100 msec * 100 pkts ~ 10 seconds, meaning this needs to be within 20%
-	require.WithinDuration(t, base.Add(duration), base.Add(time.Duration(numPkts)*time.Duration(averageMsec)*time.Millisecond), time.Duration(float64(numPkts)*0.2*float64(averageMsec))*time.Millisecond)
-}
