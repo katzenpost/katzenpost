d4934e870436bc9f04c475a62a83ae773ed8787b
Jacob Appelbaum <jacob@appelbaum.net>
2026-09-25 21:55:20 +0200

    authority/voting/config: read our identity public key in one call instead of leaving the file open, which leaked a descriptor on every FixupAndValidate and left windows unable to delete it

--- authority/voting/server/config/config_impl.go
+++ authority/voting/server/config/config_impl.go
@@ -23,7 +23,6 @@ package config
 import (
 	"errors"
 	"fmt"
-	"io"
 	"net/url"
 	"os"
 	"path/filepath"
@@ -360,11 +359,7 @@ func (cfg *Config) FixupAndValidate(forceGenOnly bool) error {
 	selfInAuthorities := false
 
 	ourPubKeyFile := filepath.Join(cfg.Server.DataDir, "identity.public.pem")
-	f, err := os.Open(ourPubKeyFile)
-	if err != nil {
-		return err
-	}
-	pemData, err := io.ReadAll(f)
+	pemData, err := os.ReadFile(ourPubKeyFile)
 	if err != nil {
 		return err
 	}
